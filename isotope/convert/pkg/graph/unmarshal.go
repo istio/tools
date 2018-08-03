0ec1497e1293d1b858e857a461ebdcd078e4f446
Tyler Berry <tyler@berryac.com>
2018-08-03 10:44:48 -0700

    Add files in pkg graph for unmarshaling a topology

---
+++ isotope/convert/pkg/graph/unmarshal.go
@@ -0,0 +1,112 @@
+// Copyright 2018 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this currentFile except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package graph
+
+import (
+	"encoding/json"
+	"sync"
+
+	"istio.io/tools/isotope/convert/pkg/graph/pct"
+	"istio.io/tools/isotope/convert/pkg/graph/script"
+	"istio.io/tools/isotope/convert/pkg/graph/size"
+	"istio.io/tools/isotope/convert/pkg/graph/svc"
+	"istio.io/tools/isotope/convert/pkg/graph/svctype"
+)
+
+// UnmarshalJSON converts b into a valid ServiceGraph. See validate() for the
+// details on what it means to be "valid".
+func (g *ServiceGraph) UnmarshalJSON(b []byte) (err error) {
+	metadata := serviceGraphJSONMetadata{Defaults: defaultDefaults}
+	err = json.Unmarshal(b, &metadata)
+	if err != nil {
+		return
+	}
+
+	*g, err = parseJSONServiceGraphWithDefaults(b, metadata.Defaults)
+	if err != nil {
+		return
+	}
+
+	err = validate(*g)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func parseJSONServiceGraphWithDefaults(
+	b []byte, defaults defaults) (sg ServiceGraph, err error) {
+	withGlobalDefaults(defaults, func() {
+		var unmarshallable unmarshallableServiceGraph
+		innerErr := json.Unmarshal(b, &unmarshallable)
+		if innerErr == nil {
+			sg = ServiceGraph(unmarshallable)
+		} else {
+			err = innerErr
+		}
+	})
+	return
+}
+
+// defaultDefaults is a stuttery but validly semantic name for the default
+// values when parsing JSON defaults.
+var (
+	defaultDefaults = defaults{
+		Type:        svctype.ServiceHTTP,
+		NumReplicas: 1,
+	}
+	defaultMutex sync.Mutex
+)
+
+type serviceGraphJSONMetadata struct {
+	Defaults defaults `json:"defaults"`
+}
+
+type defaults struct {
+	Type         svctype.ServiceType `json:"type"`
+	NumReplicas  int32               `json:"numReplicas"`
+	ErrorRate    pct.Percentage      `json:"errorRate"`
+	ResponseSize size.ByteSize       `json:"responseSize"`
+	Script       script.Script       `json:"script"`
+	RequestSize  size.ByteSize       `json:"requestSize"`
+}
+
+func withGlobalDefaults(defaults defaults, f func()) {
+	defaultMutex.Lock()
+
+	origDefaultService := svc.DefaultService
+	svc.DefaultService = svc.Service{
+		Type:         defaults.Type,
+		NumReplicas:  defaults.NumReplicas,
+		ErrorRate:    defaults.ErrorRate,
+		ResponseSize: defaults.ResponseSize,
+		Script:       defaults.Script,
+	}
+
+	origDefaultRequestCommand := script.DefaultRequestCommand
+	script.DefaultRequestCommand = script.RequestCommand{
+		Size: defaults.RequestSize,
+	}
+
+	f()
+
+	svc.DefaultService = origDefaultService
+	script.DefaultRequestCommand = origDefaultRequestCommand
+
+	defaultMutex.Unlock()
+}
+
+type unmarshallableServiceGraph ServiceGraph
---
+++ isotope/convert/pkg/graph/unmarshal_test.go
@@ -0,0 +1,192 @@
+// Copyright 2018 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this currentFile except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package graph
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+	"time"
+
+	"istio.io/tools/isotope/convert/pkg/graph/script"
+	"istio.io/tools/isotope/convert/pkg/graph/svc"
+
+	"istio.io/tools/isotope/convert/pkg/graph/svctype"
+)
+
+func TestServiceGraph_UnmarshalJSON(t *testing.T) {
+	tests := []struct {
+		input []byte
+		graph ServiceGraph
+		err   error
+	}{
+		{jsonWithOneService, graphWithOneService, nil},
+		{jsonWithDefaultsAndManyServices, graphWithDefaultsAndManyServices, nil},
+		{
+			jsonWithRequestToUndefinedService,
+			ServiceGraph{},
+			ErrRequestToUndefinedService{"b"},
+		},
+		{
+			jsonWithNestedConcurrentCommand,
+			ServiceGraph{},
+			ErrNestedConcurrentCommand,
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run("", func(t *testing.T) {
+			t.Parallel()
+
+			var graph ServiceGraph
+			err := json.Unmarshal(test.input, &graph)
+			if err == nil {
+				if !reflect.DeepEqual(test.graph, graph) {
+					t.Errorf("expected %v; actual %v", test.graph, graph)
+				}
+			} else {
+				if test.err != err {
+					t.Errorf("expected %v; actual %v", test.err, err)
+				}
+			}
+		})
+	}
+}
+
+var (
+	jsonWithOneService = []byte(`
+		{
+			"services": [{"name": "a"}]
+		}
+	`)
+	graphWithOneService = ServiceGraph{[]svc.Service{
+		{
+			Name:        "a",
+			Type:        svctype.ServiceHTTP,
+			NumReplicas: 1,
+		},
+	}}
+	jsonWithDefaultsAndManyServices = []byte(`
+		{
+			"defaults": {
+				"errorRate": 0.1,
+				"numReplicas": 2,
+				"requestSize": 516,
+				"responseSize": 128,
+				"script": [
+					{ "sleep": "100ms" }
+				]
+			},
+			"services": [
+				{
+					"name": "a",
+					"numReplicas": 5
+				},
+				{
+					"name": "b",
+					"script": [
+						{
+							"call": {
+								"service": "a",
+								"size": "1KiB"
+							}
+						},
+						{ "sleep": "10ms" }
+					]
+				},
+				{
+					"name": "c",
+					"type": "grpc",
+					"numReplicas": 1,
+					"errorRate": "20%",
+					"responseSize": "1K",
+					"script": [
+						[
+							{ "call": "a" },
+							{ "call": "b" }
+						],
+						{ "sleep": "10ms" }
+					]
+				}
+			]
+		}
+	`)
+	graphWithDefaultsAndManyServices = ServiceGraph{[]svc.Service{
+		{
+			Name:         "a",
+			Type:         svctype.ServiceHTTP,
+			NumReplicas:  5,
+			ErrorRate:    0.1,
+			ResponseSize: 128,
+			Script: script.Script([]script.Command{
+				script.SleepCommand(100 * time.Millisecond),
+			}),
+		},
+		{
+			Name:         "b",
+			Type:         svctype.ServiceHTTP,
+			NumReplicas:  2,
+			ErrorRate:    0.1,
+			ResponseSize: 128,
+			Script: script.Script([]script.Command{
+				script.RequestCommand{ServiceName: "a", Size: 1024},
+				script.SleepCommand(10 * time.Millisecond),
+			}),
+		},
+		{
+			Name:         "c",
+			Type:         svctype.ServiceGRPC,
+			NumReplicas:  1,
+			ErrorRate:    0.2,
+			ResponseSize: 1024,
+			Script: script.Script([]script.Command{
+				script.ConcurrentCommand{
+					script.RequestCommand{ServiceName: "a", Size: 516},
+					script.RequestCommand{ServiceName: "b", Size: 516},
+				},
+				script.SleepCommand(10 * time.Millisecond),
+			}),
+		},
+	}}
+	jsonWithRequestToUndefinedService = []byte(`
+		{
+			"services": [
+				{
+					"name": "a",
+					"script": [{ "call": "b"}]
+				}
+			]
+		}
+	`)
+	jsonWithNestedConcurrentCommand = []byte(`
+		{
+			"services": [
+				{
+					"name": "a"
+				},
+				{
+					"name": "b",
+					"script": [
+						[
+							[{ "call": "a" }, { "call": "a" }],
+							{ "sleep": "10ms" }
+						]
+					]
+				}
+			]
+		}
+	`)
+)
