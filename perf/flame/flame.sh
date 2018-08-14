8f78f8132f8301006c1020388bf4f1deb54d53c6
mandarjog <mandarjog@gmail.com>
2018-08-13 18:16:56 -0700

    Envoy flame Graphs: Doc and scripts (#9)
    * Add README and scripts to get cpu flame graphs
    
    * use a consistent color palette
    
    * add inline svg
    
    * review comments

---
+++ perf/flame/flame.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+WD=$(dirname "${0}")
+WD=$(cd "${WD}" && pwd)
+
+FLAMEDIR="${WD}/FlameGraph"
+
+if ! command -v c++filt > /dev/null; then
+    echo "Install c++filt to demangle symbols"
+    exit 1
+fi 
+
+cd "${WD}" || exit -1
+
+if [[ ! -d ${FLAMEDIR} ]]; then
+    echo "Cloning FlameGraph repo in ${WD}"
+    git clone https://github.com/brendangregg/FlameGraph
+fi
+
+# Given output of `perf script` produce a flamegraph
+FILE=${1:?"perf script output"}
+FILENAME=$(basename "${FILE}")
+BASE=$(echo "${FILENAME}" | cut -d '.' -f 1)
+SVGNAME="${BASE}.svg"
+
+"${FLAMEDIR}/stackcollapse-perf.pl" "${FILE}" | c++filt -n | "${FLAMEDIR}/flamegraph.pl" --cp > "${SVGNAME}"
+
+echo "Wrote ${SVGNAME}"
+if [[ ! -z "${BUCKET}" ]];then
+    gsutil cp "${SVGNAME}" "${BUCKET}"
+fi
