ee8e1ecc7591dd4c53855de16cdcb9e4f3266929
Tyler Berry <tyler@berryac.com>
2018-08-16 17:04:41 -0700

    Isotope: Add containerized service application (#8)
    * Add containerized service application
    
    * Rename "service" to "isotope_service"
    
    * Remove naked return in serveWithPrometheus
    
    * Move "istio.io/fortio/log" to top of third party imports
    
    * Update to istio.io/fortio:v1.1.0
    
    * Remove explicit log level setting (allows setting by flag)
    
    * Update performance documentation

---
+++ isotope/service/main.go
@@ -0,0 +1,95 @@
+// Copyright 2018 Istio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this currentFile except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"path"
+	"runtime"
+
+	"istio.io/fortio/log"
+	"istio.io/tools/isotope/convert/pkg/consts"
+	"istio.io/tools/isotope/service/pkg/srv"
+	"istio.io/tools/isotope/service/pkg/srv/prometheus"
+)
+
+const (
+	promEndpoint    = "/metrics"
+	defaultEndpoint = "/"
+)
+
+var (
+	serviceGraphYAMLFilePath = path.Join(
+		consts.ConfigPath, consts.ServiceGraphYAMLFileName)
+
+	// Set by the Python script when running this through Docker.
+	maxIdleConnectionsPerHostFlag = flag.Int(
+		"max-idle-connections-per-host", 0,
+		"maximum number of TCP connections to keep open per host")
+)
+
+func main() {
+	flag.Parse()
+
+	setMaxProcs()
+	setMaxIdleConnectionsPerHost(*maxIdleConnectionsPerHostFlag)
+
+	serviceName, ok := os.LookupEnv(consts.ServiceNameEnvKey)
+	if !ok {
+		log.Fatalf(`env var "%s" is not set`, consts.ServiceNameEnvKey)
+	}
+
+	defaultHandler, err := srv.HandlerFromServiceGraphYAML(
+		serviceGraphYAMLFilePath, serviceName)
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+
+	err = serveWithPrometheus(defaultHandler)
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+}
+
+func serveWithPrometheus(defaultHandler http.Handler) error {
+	log.Infof(`exposing Prometheus endpoint "%s"`, promEndpoint)
+	http.Handle(promEndpoint, prometheus.Handler())
+
+	log.Infof(`exposing default endpoint "%s"`, defaultEndpoint)
+	http.Handle(defaultEndpoint, defaultHandler)
+
+	addr := fmt.Sprintf(":%d", consts.ServicePort)
+	log.Infof("listening on port %v\n", consts.ServicePort)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		return err
+	}
+	return nil
+}
+
+func setMaxProcs() {
+	numCPU := runtime.NumCPU()
+	maxProcs := runtime.GOMAXPROCS(0)
+	if maxProcs < numCPU {
+		log.Infof("setting GOMAXPROCS to %v (previously %v)", numCPU, maxProcs)
+		runtime.GOMAXPROCS(numCPU)
+	}
+}
+
+func setMaxIdleConnectionsPerHost(n int) {
+	http.DefaultTransport.(*http.Transport).MaxIdleConnsPerHost = n
+}
