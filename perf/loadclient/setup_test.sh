99c0b634d752687748c66e20e53e8154dd7fba20
Mandar U Jog <mjog@google.com>
2018-10-12 00:05:21 +0000

    Add large topology with its load generator

--- perf/loadclient/setup_test.sh
+++ perf/loadclient/setup_test.sh
@@ -3,16 +3,15 @@ set -ex
 NAMESPACE=${1:?"namespace"}
 NAMEPREFIX=${2:?"prefix name for service. typically svc-"}
 
-# Get pod ip range, there must be a better way, but this works.
-function ip_range() {
-    kubectl get pods --namespace kube-system -o wide | grep kube-dns | awk '{print $6}'|head -1 | awk -F '.' '{printf "%s.%s.0.0/16\n", $1, $2}'
-}
+GATEWAY_URL=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+SERVICEHOST="${NAMEPREFIX}0"
 
 function run_test() {
   YAML=$(mktemp).yml
   helm -n ${NAMESPACE} template \
-	  --set serviceNamePrefix="${NAMEPREFIX}" \
+	  --set serviceHost="${NAMEPREFIX}0" \
     --set Namespace="${NAMESPACE}" \
+    --set ingress="${GATEWAY_URL}" \
           . > "${YAML}"
   echo "Wrote ${YAML}"
 
