b81520175e8308084e6a82b4473320eb128b7139
Mandar U Jog <mjog@google.com>
2018-11-14 21:57:27 +0000

    add domain

--- perf/twoPodTest/setup_test.sh
+++ perf/twoPodTest/setup_test.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-set -e
+WD=$(dirname $0)
+WD=$(cd "${WD}"; pwd)
+cd "${WD}"
+
+set -ex
 NAMESPACE=${NAMESPACE:?"namespace"}
+DNS_DOMAIN=${DNS_DOMAIN:?"DNS_DOMAIN like v104.qualistio.org or local"}
+TMPDIR=${TMPDIR:-${WD}/tmp}
+
+mkdir -p "${TMPDIR}"
 
 # Get pod ip range, there must be a better way, but this works.
 function ip_range() {
@@ -10,12 +18,13 @@ function ip_range() {
 function run_test() {
   helm -n ${NAMESPACE} template \
 	  --set excludeOutboundIPRanges=$(ip_range) \
+    --set domain="${DNS_DOMAIN}" \
           . > ${TMPDIR}/twopods.yaml
   echo "Wrote ${TMPDIR}/twopods.yaml"
 
   # remove stdio rules
-  kubectl --namespace istio-system delete rules stdio stdiotcp
-#         kubectl apply -n ${NAMESPACE} -f ${TMPDIR}/twopods.yaml
+  kubectl --namespace istio-system delete rules stdio stdiotcp || true
+  kubectl apply -n ${NAMESPACE} -f ${TMPDIR}/twopods.yaml
 }
 
 run_test
