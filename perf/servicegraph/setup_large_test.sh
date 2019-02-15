c336a72e5d1ae7c5ebc354d5c767f68862f87ad0
Maria Skidanova <skidam@google.com>
2019-02-15 12:38:25 -0800

    Added Common.sh and deleted setup_test.sh from servicegraph

--- perf/servicegraph/setup_large_test.sh
+++ perf/servicegraph/setup_large_test.sh
@@ -8,6 +8,8 @@ cd "${WD}"
 
 set -ex
 
+source common.sh
+
 NUM=${1:?"number of namespaces. 20 x this number"}
 START=${2:-"0"}
 
@@ -16,23 +18,5 @@ CMD=""
 if [[ ! -z "${ECHO}" ]];then
   CMD="echo" 
 fi
-
-function start_servicegraphs() {
-  local nn=${1:?"number of namespaces"}
-  local min=${2:-"0"}
-
-  for ((ii=$min; ii<$nn; ii++)) {
-    ns=$(printf 'service-graph%.2d' $ii)
-    prefix=$(printf 'svc%.2d-' $ii)
-    
-    if [[ -z "${DELETE}" ]];then
-      ${CMD} "${WD}/setup_test.sh" "${ns}" "${prefix}"
-      ${CMD} "${WD}/../loadclient/setup_test.sh" "${ns}" "${prefix}"
-    else
-      ${CMD} "${WD}/../loadclient/setup_test.sh" "${ns}" "${prefix}"
-      ${CMD} "${WD}/setup_test.sh" "${ns}" "${prefix}"
-    fi    
-  }
-}
 
 start_servicegraphs "${NUM}" "${START}"
