34c9a3f264013f8a32264992af4554794bd29138
Maria Skidanova <skidam@google.com>
2019-02-15 12:26:30 -0800

    git mv

--- perf/servicegraph/common.sh
+++ perf/servicegraph/common.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-function run_test() {
+ function run_test() {
   local ns=${1:?"namespaces"}
   local prefix=${2:?"prefix name for service. typically svc-"}
 
-  YAML=$(mktemp).yml
+   YAML=$(mktemp).yml
   helm -n ${ns} template \
           --set serviceNamePrefix="${prefix}" \
           --set Namespace="${ns}" \
           . > "${YAML}"
   echo "Wrote ${YAML}"
 
-  kubectl create ns "${ns}" || true
+   kubectl create ns "${ns}" || true
   kubectl label namespace "${ns}" istio-injection=enabled --overwrite
 
-  # remove stdio rules
+   # remove stdio rules
   kubectl --namespace istio-system delete rules stdio stdiotcp || true
 
-  if [[ -z "${DELETE}" ]];then
+   if [[ -z "${DELETE}" ]];then
     sleep 3
     kubectl -n "${ns}" apply -f "${YAML}"
   else
@@ -26,11 +26,11 @@ function run_test() {
   fi
 }
 
-function start_servicegraphs() {
+ function start_servicegraphs() {
   local nn=${1:?"number of namespaces"}
   local min=${2:?"from which namespace to start"}
 
-  for ((ii=$min; ii<$nn; ii++)) {
+   for ((ii=$min; ii<$nn; ii++)) {
     ns=$(printf 'service-graph%.2d' $ii)
     prefix=$(printf 'svc%.2d-' $ii)
     if [[ -z "${DELETE}" ]];then
@@ -43,7 +43,7 @@ function start_servicegraphs() {
   }
 }
 
-# Get pod ip range, there must be a better way, but this works.
+ # Get pod ip range, there must be a better way, but this works.
 function ip_range() {
     kubectl get pods --namespace kube-system -o wide | grep kube-dns | awk '{print $6}'|head -1 | awk -F '.' '{printf "%s.%s.0.0/16\n", $1, $2}'
 }
