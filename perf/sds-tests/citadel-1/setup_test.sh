03c00e2a37be1d446b798a89da58752e5d5217c2
lei-tang <32078630+lei-tang@users.noreply.github.com>
2019-03-06 16:37:28 -0800

    Enable collecting test stats at any time (#71)

--- perf/sds-tests/citadel-1/setup_test.sh
+++ perf/sds-tests/citadel-1/setup_test.sh
@@ -23,40 +23,12 @@ function inject_workload() {
 TEMP_DEPLOY_NAME="temp_httpbin_sleep_deploy.yaml"
 helm template --set replicas="${NUM}" .. > "${TEMP_DEPLOY_NAME}"
 
+kubectl create ns ${NAMESPACE}
+
 inject_workload ${TEMP_DEPLOY_NAME}
 
-echo "Wait 120 seconds for the deployment to be ready ..."
-sleep 120
+echo "Wait 90 seconds for the deployment to be ready ..."
+sleep 90
 
 
-echo "Run curl to test certificate rotations and mTLS"
-num_curl=0
-num_succeed=0
-
-while [ 1 ]
-do
-  sleep_pods=$(kubectl get pods -n ${NAMESPACE} -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' -l app=sleep)
-  pods=()
-
-  while read -r line; do
-    pods+=("$line")
-  done <<< "${sleep_pods}"
-
-  if [ ${#pods[@]} = 0 ]; then
-    echo "no pods found!"
-  fi
-
-  for pod in "${pods[@]}"
-  do
-    resp_code=$(kubectl exec -it -n ${NAMESPACE} "${pod}" -c sleep -- curl -s -o /dev/null -w "%{http_code}" httpbin:8000/headers)
-    if [ ${resp_code} = 200 ]; then
-      num_succeed=$((num_succeed+1))
-    else
-      echo "curl from the pod ${pod} failed"
-    fi
-    num_curl=$((num_curl+1))
-    echo "Out of ${num_curl} curl, ${num_succeed} succeeded."
-    sleep 1
-  done
-
-done
+source ./collect_stats.sh
