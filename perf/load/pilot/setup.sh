1a4fa32d4a0c13454de4602b04db0f2db2911ea4
John Howard <howardjohn@google.com>
2019-03-14 16:22:22 -0700

    inital pilot load testing

---
+++ perf/load/pilot/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#set -ex
+
+WD=$(dirname $0)
+WD=$(cd $WD; pwd)
+
+function setup_test() {
+  local NAMESPACE="${NAMESPACE:-"pilot-load"}"
+
+  mkdir -p "${WD}/tmp"
+  local OUTFILE="${WD}/tmp/${NAMESPACE}.yaml"
+
+  kubectl create ns "${NAMESPACE}" || true
+  kubectl label namespace "${NAMESPACE}" istio-injection=enabled || true
+
+  helm --namespace "${NAMESPACE}" template "${WD}" > "${OUTFILE}"
+
+  if [[ -z "${DRY_RUN}" ]]; then
+      kubectl --namespace "${NAMESPACE}" apply -f "${OUTFILE}"
+  fi
+}
+
+setup_test
