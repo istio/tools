70b9c0d1837728c1434b8702f5930a4016115510
Mitch Connors <mitchconnors@gmail.com>
2019-05-13 12:32:41 -0700

    Add serviceaccount definition for updater.

--- perf/auto-qual-test/deploy_updater.sh
+++ perf/auto-qual-test/deploy_updater.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # this will look much better once we have kubectl 1.14 with kustomize support
 kubectl create configmap qual-test-deployer --from-file=deploy_latest_daily.sh
-kubectl apply -f qual-test-update-job.yaml
+kubectl apply -f .
