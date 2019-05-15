7e06e77c9d0a277801b0a98e4d7de326027f5b95
Mitch Connors <mitchconnors@gmail.com>
2019-05-15 10:34:46 -0700

    Move 'release' into target_version variable

--- perf/auto-qual-test/deploy_latest_daily.sh
+++ perf/auto-qual-test/deploy_latest_daily.sh
@@ -8,7 +8,7 @@ git clone https://github.com/istio/tools
 cd tools/perf/istio-install
 helm init --client-only
 TARGET_VERSION=$1
-LATEST_BUILD=$(curl -L https://gcsweb.istio.io/gcs/istio-prerelease/daily-build/release-$TARGET_VERSION-latest.txt)
+LATEST_BUILD=$(curl -L https://gcsweb.istio.io/gcs/istio-prerelease/daily-build/$TARGET_VERSION-latest.txt)
 HELMREPO_URL=https://gcsweb.istio.io/gcs/istio-prerelease/daily-build/$LATEST_BUILD/charts/ RELEASE_URL=https://gcsweb.istio.io/gcs/istio-prerelease/daily-build/$LATEST_BUILD/istio-$LATEST_BUILD-linux.tar.gz DNS_DOMAIN=v11.qualistio.org ./setup_istio.sh $TARGET_VERSION
 
 # trigger redeploy on services to get new sidecars
