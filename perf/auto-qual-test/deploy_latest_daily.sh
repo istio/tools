82b0dae44c0a6dc0ede37524ea9cf4ca9086c23e
Mitch Connors <mitchconnors@gmail.com>
2019-05-16 04:38:55 -0700

    Add automated qual test job (#126)
    * Add automated qual test job
    
    * Remove duplicate redeploy script from qual script
    
    * Add serviceaccount definition for updater.
    
    * Add working directory prologue
    
    * Use WD for local script calls
    
    * Move 'release' into target_version variable
    
    * Update variable for deploy job

---
+++ perf/auto-qual-test/deploy_latest_daily.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -ex
+
+WD=$(dirname $0)
+WD=$(cd $WD; pwd)
+
+git clone https://github.com/istio/tools
+cd tools/perf/istio-install
+helm init --client-only
+TARGET_VERSION=$1
+LATEST_BUILD=$(curl -L https://gcsweb.istio.io/gcs/istio-prerelease/daily-build/$TARGET_VERSION-latest.txt)
+HELMREPO_URL=https://gcsweb.istio.io/gcs/istio-prerelease/daily-build/$LATEST_BUILD/charts/ RELEASE_URL=https://gcsweb.istio.io/gcs/istio-prerelease/daily-build/$LATEST_BUILD/istio-$LATEST_BUILD-linux.tar.gz DNS_DOMAIN=v11.qualistio.org ./setup_istio.sh $TARGET_VERSION
+
+# trigger redeploy on services to get new sidecars
+${WD}/../../bin/redeploy.sh ALL
