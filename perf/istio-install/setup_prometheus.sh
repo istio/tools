0a0a1afc3b1851e1c3ec3222a15f526550ce6ffb
Mitch Connors <mitchconnors@gmail.com>
2019-05-20 14:49:20 -0700

    Apply prometheus CRDs before applying CRs

--- perf/istio-install/setup_prometheus.sh
+++ perf/istio-install/setup_prometheus.sh
@@ -7,6 +7,7 @@ mkdir -p "${WD}/tmp"
 
 function install_prometheus() {
   local DIRNAME="$1" # should be like tools/perf/istio/tmp
+  DIRNAME=$(cd $DIRNAME; pwd)
   local PROMETHEUS_INSTALL="${DIRNAME}"
 
   # Create GCP SSD Storage class for Prometheus to use. May not work outside GKE
@@ -15,16 +16,25 @@ function install_prometheus() {
   helm fetch stable/prometheus-operator --untar --untardir "${PROMETHEUS_INSTALL}"
 
   kubectl create ns istio-prometheus || true
-  PROM_OP="$DIRNAME/prom-operator.yaml"
+  PROM_OP="${DIRNAME}prom-operator.yaml"
+  PROM_CRDS="${DIRNAME}prom-op-crds.yaml"
+  cd "${PROMETHEUS_INSTALL}/prometheus-operator"
+  ls ./templates/prometheus-operator/crd*.yaml | \
+    sed -n 's/^/-x /p' | \
+    xargs helm template ./ \
+      --namespace istio-prometheus \
+      --name prometheus \
+      -f ${WD}/../prometheus-install/values.yaml > "${PROM_CRDS}"
   helm template "${PROMETHEUS_INSTALL}/prometheus-operator/" \
     --namespace istio-prometheus \
     --name prometheus \
     -f "${WD}/../prometheus-install/values.yaml" > "${PROM_OP}"
 
+  echo "${PROM_CRDS}"
   echo "${PROM_OP}"
 
-
   if [[ -z "${DRY_RUN}" ]]; then
+    kubectl apply -f "${PROM_CRDS}"
     kubectl apply --namespace istio-prometheus -f "${PROM_OP}"
 
     # delete grafana pod so it redeploys with new config
