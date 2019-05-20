aa8f76934761003c993ea22aafde06da203446ba
Mitch Connors <mitchconnors@gmail.com>
2019-05-20 12:50:09 -0700

    Package redeploy script in container (#143)

--- perf/auto-qual-test/deploy_latest_daily.sh
+++ perf/auto-qual-test/deploy_latest_daily.sh
@@ -7,9 +7,12 @@ WD=$(cd $WD; pwd)
 git clone https://github.com/istio/tools
 cd tools/perf/istio-install
 helm init --client-only
-TARGET_VERSION=$1
-LATEST_BUILD=$(curl -L https://gcsweb.istio.io/gcs/istio-prerelease/daily-build/$TARGET_VERSION-latest.txt)
-HELMREPO_URL=https://gcsweb.istio.io/gcs/istio-prerelease/daily-build/$LATEST_BUILD/charts/ RELEASE_URL=https://gcsweb.istio.io/gcs/istio-prerelease/daily-build/$LATEST_BUILD/istio-$LATEST_BUILD-linux.tar.gz DNS_DOMAIN=v11.qualistio.org ./setup_istio.sh $TARGET_VERSION
+RELEASE_MAJOR_MINOR="release-1.1"
+if [[ ! -z "${TARGET_VERSION}" ]];then
+  RELEASE_MAJOR_MINOR="${TARGET_VERSION}"
+fi
+LATEST_BUILD=$(curl -L https://gcsweb.istio.io/gcs/istio-prerelease/daily-build/$RELEASE_MAJOR_MINOR-latest.txt)
+HELMREPO_URL=https://gcsweb.istio.io/gcs/istio-prerelease/daily-build/$LATEST_BUILD/charts/ RELEASE_URL=https://gcsweb.istio.io/gcs/istio-prerelease/daily-build/$LATEST_BUILD/istio-$LATEST_BUILD-linux.tar.gz DNS_DOMAIN=v11.qualistio.org ./setup_istio.sh $RELEASE_MAJOR_MINOR
 
 # trigger redeploy on services to get new sidecars
-${WD}/../../bin/redeploy.sh ALL
+/etc/scripts/redeploy.sh ALL
