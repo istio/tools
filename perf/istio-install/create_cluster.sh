6d0db243bf67aa75207be14494a355ae3aff649f
Mandar U Jog <mjog@google.com>
2019-05-20 01:17:14 +0000

    Remove nodepool and consolidate options

--- perf/istio-install/create_cluster.sh
+++ perf/istio-install/create_cluster.sh
@@ -1,17 +1,49 @@
 #!/bin/bash
-
+# set -x
 # Creates a standard cluster for testing.
-# You may adjust the initial size of the node pool
 
+# get default cluster version for zone
+function default_cluster() {
+  local zone=${1:?"zone required"}
+  local temp_ver=$(mktemp)
+
+  gcloud container get-server-config --zone "${zone}"  >${temp_ver} 2>&1
+  if [[ $? -ne 0 ]];then
+    cat "${temp_ver}"
+    exit 1
+  fi
+
+  ver=$(cat "${temp_ver}" | grep defaultClusterVersion | awk '{print $2}')
+  echo "${ver}"
+  rm -Rf "${temp_ver}"
+}
+
+# Required params
 PROJECT_ID=${PROJECT_ID:?"project id is required"}
 CLUSTER_NAME=${1:?"cluster name"}
+
+# Optional params
 ZONE=${ZONE:-us-central1-a}
-DEFAULT_VERSION=$(gcloud container get-server-config --zone us-central1-a  2>/dev/null | grep defaultClusterVersion | awk '{print $2}')
-GKE_VERSION=${GKE_VERSION-${DEFAULT_VERSION}}
-SCOPES="https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append"
-MAXPODS_PER_NODE=100
+
+# Sizing
+DISK_SIZE=${DISK_SIZE:-64}
+MACHINE_TYPE=${MACHINE_TYPE:-n1-standard-32}
 MIN_NODES=${MIN_NODES:-"4"}
 MAX_NODES=${MAX_NODES:-"70"}
+
+MAXPODS_PER_NODE=100
+
+# Labels and version
+ISTIO_VERSION=${ISTIO_VERSION:?"Istio version label"}
+DEFAULT_GKE_VERSION=$(default_cluster "${ZONE}")
+if [[ $? -ne 0 ]];then
+  echo "${DEFAULT_GKE_VERSION}"
+  exit 1
+fi
+GKE_VERSION=${GKE_VERSION-${DEFAULT_GKE_VERSION}}
+SCOPES="https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append"
+# A label cannot have "." in it.
+ISTIO_VERSION=$(echo ${ISTIO_VERSION} | sed 's/\./_/g')
 
 function gc() {
   echo $*
@@ -28,17 +60,11 @@ gc beta container \
   clusters create "${CLUSTER_NAME}" \
   --zone "${ZONE}" \
   --no-enable-basic-auth --cluster-version "${GKE_VERSION}" \
-  --machine-type "n1-standard-32" --image-type "COS" --disk-type "pd-standard" --disk-size "64" \
+  --machine-type "${MACHINE_TYPE}" --image-type "COS" --disk-type "pd-standard" --disk-size "${DISK_SIZE}" \
   --scopes "${SCOPES}" \
-  --max-pods-per-node "${MAXPODS_PER_NODE}" --num-nodes "1" --enable-stackdriver-kubernetes --enable-ip-alias --create-subnetwork name="${CLUSTER_NAME}-subnet" \
+  --num-nodes "${MIN_NODES}" --enable-autoscaling --min-nodes "${MIN_NODES}" --max-nodes "${MAX_NODES}" --max-pods-per-node "${MAXPODS_PER_NODE}" \
+  --enable-stackdriver-kubernetes \
+  --enable-ip-alias --create-subnetwork name="${CLUSTER_NAME}-subnet" \
   --default-max-pods-per-node "${MAXPODS_PER_NODE}" \
   --addons HorizontalPodAutoscaling,HttpLoadBalancing,KubernetesDashboard \
-  --enable-network-policy --enable-autoupgrade --enable-autorepair --labels test-date=$(date +%Y-%m-%d),version=v12,operator=user_${USER}
-
-gc beta container \
-  --project "${PROJECT_ID}" \
-  node-pools create "service-graph-pool" --cluster "${CLUSTER_NAME}" --zone "${ZONE}" \
-  --node-version "${GKE_VERSION}" \
-  --machine-type "n1-standard-32" --image-type "COS" --disk-type "pd-standard" --disk-size "64" \
-  --scopes "${SCOPES}" \
-  --num-nodes "${MIN_NODES}" --enable-autoscaling --min-nodes "${MIN_NODES}" --max-nodes "${MAX_NODES}" --no-enable-autoupgrade --enable-autorepair --max-pods-per-node "${MAXPODS_PER_NODE}"
+  --enable-network-policy --enable-autoupgrade --enable-autorepair --labels test-date=$(date +%Y-%m-%d),version=${ISTIO_VERSION},operator=user_${USER}
