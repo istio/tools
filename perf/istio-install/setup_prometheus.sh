6cc9ed7b892ec1f5e224b8cd200fb987ba348013
Mitch Connors <mitchconnors@gmail.com>
2019-05-21 16:18:20 -0700

    Apply prometheus CRDs before applying CRs (#145)
    * Apply prometheus CRDs before applying CRs
    
    * Update to use prom-operator`s preferred CRD yaml
    
    * Revert "Rerun apply crds on perf clusters (#135)"
    
    This reverts commit 268f0478681c0002623055c437dac6ef129512fd.

--- perf/istio-install/setup_prometheus.sh
+++ perf/istio-install/setup_prometheus.sh
@@ -7,6 +7,7 @@ mkdir -p "${WD}/tmp"
 
 function install_prometheus() {
   local DIRNAME="$1" # should be like tools/perf/istio/tmp
+  DIRNAME=$(cd $DIRNAME; pwd)
   local PROMETHEUS_INSTALL="${DIRNAME}"
 
   # Create GCP SSD Storage class for Prometheus to use. May not work outside GKE
@@ -15,7 +16,7 @@ function install_prometheus() {
   helm fetch stable/prometheus-operator --untar --untardir "${PROMETHEUS_INSTALL}"
 
   kubectl create ns istio-prometheus || true
-  PROM_OP="$DIRNAME/prom-operator.yaml"
+  PROM_OP="${DIRNAME}prom-operator.yaml"
   helm template "${PROMETHEUS_INSTALL}/prometheus-operator/" \
     --namespace istio-prometheus \
     --name prometheus \
@@ -23,8 +24,17 @@ function install_prometheus() {
 
   echo "${PROM_OP}"
 
-
   if [[ -z "${DRY_RUN}" ]]; then
+    kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/alertmanager.crd.yaml
+    kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/prometheus.crd.yaml
+    kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/prometheusrule.crd.yaml
+    kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/servicemonitor.crd.yaml
+
+    kubectl wait --for=condition=Established -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/alertmanager.crd.yaml
+    kubectl wait --for=condition=Established -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/prometheus.crd.yaml
+    kubectl wait --for=condition=Established -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/prometheusrule.crd.yaml
+    kubectl wait --for=condition=Established -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/servicemonitor.crd.yaml
+
     kubectl apply --namespace istio-prometheus -f "${PROM_OP}"
 
     # delete grafana pod so it redeploys with new config
