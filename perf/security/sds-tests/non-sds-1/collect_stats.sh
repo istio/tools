8ccca3de45f71b63aeeaa4c075cc4b16043c1aa9
Lei Tang <32078630+lei-tang@users.noreply.github.com>
2019-05-29 17:27:12 -0700

    Add CLUSTER parameter for non-SDS tests to specify the cluster to run a test

--- perf/security/sds-tests/non-sds-1/collect_stats.sh
+++ perf/security/sds-tests/non-sds-1/collect_stats.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 NAMESPACE=${NAMESPACE:?"specify the namespace for running the test"}
+CLUSTER=${CLUSTER:?"specify the cluster for running the test"}
 
 echo "Run curl to test certificate rotations and mTLS"
 num_curl=0
@@ -9,7 +10,7 @@ num_succeed=0
 
 while [ 1 ]
 do
-  sleep_pods=$(kubectl get pods -n ${NAMESPACE} -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' -l app=sleep)
+  sleep_pods=$(kubectl get pods -n ${NAMESPACE} -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' -l app=sleep --cluster ${CLUSTER})
   pods=()
 
   while read -r line; do
@@ -22,7 +23,7 @@ do
 
   for pod in "${pods[@]}"
   do
-    resp_code=$(kubectl exec -it -n ${NAMESPACE} "${pod}" -c sleep -- curl -s -o /dev/null -w "%{http_code}" httpbin:8000/headers)
+    resp_code=$(kubectl exec -it -n ${NAMESPACE} "${pod}" -c sleep --cluster ${CLUSTER} -- curl -s -o /dev/null -w "%{http_code}" httpbin:8000/headers)
     if [ ${resp_code} = 200 ]; then
       num_succeed=$((num_succeed+1))
     else
