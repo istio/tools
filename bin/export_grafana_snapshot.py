de27af29e76bc0eaa8be12bec714eaf9a7e38188
Xinnan Wen <iamwen@google.com>
2019-05-29 17:21:14 -0700

    Add datastore to persist url, use selenium instead of grafana snapshot api to generate url

--- bin/export_grafana_snapshot.py
+++ bin/export_grafana_snapshot.py
@@ -7,9 +7,18 @@ import time
 import requests
 import requests.auth
 import schedule
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from google.cloud import datastore
 
-"""Define dashboard and snapshot related api wrapper for grafana
-   Use schedule to run export snapshot jobs periodically
+
+"""Define dashboard related api wrapper for grafana.
+   Use schedule to run export snapshot jobs periodically.
+   Due to limitation of post snapshot api, use selenium to mock action
+   of clicking share.
 """
 
 
@@ -47,7 +56,7 @@ class GrafanaAPI(object):
         if not resp.ok:
             raise Exception(resp.content)
 
-        return resp.json()
+        return resp.json()['dashboard']
 
     def search_dashboard(self, dashboard_name):
         """
@@ -58,46 +67,12 @@ class GrafanaAPI(object):
         url = self.construct_url(path)
 
         print("Search dashboard with name {0}".format(dashboard_name))
-
         resp = self.session.get(url)
-
         if not resp.ok:
             raise Exception(resp.content)
         if len(resp.json()) == 0:
             raise Exception("cannot find dashboard with name")
-        return resp.json()[0].get("uid")
-
-    def post_snapshot(self, dashboard, name="Istio Dashboard",
-                      expires=None, key=None, deletekey=None, external=False):
-        """
-        Export dashboard to snapshot
-        Snapshot name is formatted with dashboard name and timestamp
-        """
-        path = "api/snapshots"
-        if external is True:
-            if deletekey is None or key is None:
-                print("Please specify key and delete key for external snapshot")
-                exit(1)
-
-        data = dashboard
-        data['external'] = external
-        current_dt = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
-        data['name'] = name + "_" + str(current_dt)
-        if expires is not None:
-            data['expires'] = expires
-        if key is not None:
-            data['key'] = key
-        if deletekey is not None:
-            data['deletekey'] = deletekey
-
-        print("Export dashboard name: {0} as snapshot, key:{1}"
-              .format(data['name'], key))
-
-        resp = self.session.post(self.construct_url(path), json=data)
-
-        if not resp.ok:
-            raise Exception(resp.content)
-        return resp.json()
+        return resp.json()[0]
 
     def get_snapshots(self):
         """
@@ -105,7 +80,6 @@ class GrafanaAPI(object):
         """
         path = "api/dashboard/snapshots"
         url = self.construct_url(path)
-        print(url)
 
         resp = self.session.get(url)
         if not resp.ok:
@@ -133,19 +107,68 @@ class GrafanaAPI(object):
 
         return url_pattern.format(**params)
 
-    def export_snapshot(self, dashboard_name, external):
-        """
-        :param dashboard_name: dashboard to export snapshot
-        :param external: export locally or to raintank
-        """
-        dashboard_id = self.search_dashboard(dashboard_name)
-        dashboard =\
-            self.get_dashboard_by_uid(dashboard_id)
-        self.post_snapshot(dashboard,
-                           name=dashboard_name,
-                           key=dashboard_id,
-                           deletekey=dashboard_id,
-                           external=external)
+
+def persist(datastore_client, dashboard, snapshot_url, kind='Snapshot'):
+
+    # The Cloud Datastore key
+    snapshot_key = datastore_client.key(kind, dashboard['uid'])
+
+    # Prepares the new entity
+    # Can add more attributes
+    snapshot = datastore.Entity(key=snapshot_key)
+    snapshot['url'] = snapshot_url
+    snapshot['dashboard_uid'] = dashboard['uid']
+    snapshot['dashboard_name'] = dashboard['title']
+
+    snapshot['tags'] = " ".join(filter(None, dashboard['tags']))
+
+    current_dt = datetime.now().strftime('%Y-%m-%d %H:%M')
+    snapshot['timestamp'] = str(current_dt)
+
+    # Saves the entity
+    datastore_client.put(snapshot)
+
+    print('Saved snapshot url {} for dashboard {}'.
+          format(snapshot['url'], dashboard['title']))
+
+
+def export_snapshot(grafana_api, datastore_client, dashboard_name):
+    """
+    :param grafana_api: grafana api client
+    :param datastore_client: datastore api client
+    :param dashboard_name: dashboard to export snapshot
+    """
+    dashboard = grafana_api.search_dashboard(dashboard_name)
+    # remove / at beginning
+    path = dashboard['url'][1:]
+    dashboard_url = grafana_api.construct_url(path)
+
+    snapshot_url = click_share_link(dashboard_url)
+
+    persist(datastore_client, dashboard, snapshot_url)
+
+
+def click_share_link(dashboard_url):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    print("dashboard url:{0}".format(dashboard_url))
+    driver.get(dashboard_url)
+
+    element = driver.find_element_by_class_name("navbar-button--share")
+    element.click()
+
+    snapshot_tap = WebDriverWait(driver, 1).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Snapshot")))
+    snapshot_tap.click()
+
+    snapshot_share = WebDriverWait(driver, 1).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "btn-secondary")))
+    snapshot_share.click()
+    snapshot_link = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "share-modal-link")))
+    print(snapshot_link.text)
+    return snapshot_link.text
 
 
 def getParser():
@@ -153,13 +176,10 @@ def getParser():
                                      " and frequency to export snapshot")
     parser.add_argument(
         "--dashboard_name", help="Name of the dashboard to take snapshot",
-        default="istio workload")
+        default="istio performance")
 
     parser.add_argument("--period", help="Period in minutes"
-                        " to automatically export snapshots", default=5)
-
-    parser.add_argument("--external", help="False to export snapshots locally"
-                        "True to raintank", default=False)
+                        " to automatically export snapshots", default=1)
 
     return parser
 
@@ -168,10 +188,14 @@ def main(argv):
     args = getParser().parse_args(argv)
     grafana_api = GrafanaAPI()
 
+    # init datastore client
+    datastore_client = datastore.Client()
+
     # Run at schedule
-    schedule.every(args.period).minutes.do(grafana_api.export_snapshot,
-                                           args.dashboard_name,
-                                           args.external)
+    schedule.every(args.period).minutes.do(export_snapshot,
+                                           grafana_api,
+                                           datastore_client,
+                                           args.dashboard_name)
     while True:
         schedule.run_pending()
         time.sleep(1)
