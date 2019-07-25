b4119803cd1639e66fdd09cfe5cfcdb74cb00c26
Maria Skidanova <skidam@google.com>
2019-07-25 10:57:11 -0700

    Fixing undefined var in bash

--- perf/load/common.sh
+++ perf/load/common.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ -z "${GATEWAY_URL}" ]];then
+if [[ -z "${GATEWAY_URL-}" ]];then
 SYSTEM_GATEWAY_URL=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}' || true)
 INGRESS_GATEWAY_URL=$(kubectl -n istio-ingress get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}' || true)
 GATEWAY_URL=${SYSTEM_GATEWAY_URL:-$INGRESS_GATEWAY_URL}
