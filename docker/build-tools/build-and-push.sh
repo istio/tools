8b069f5a44c152e439f6552d02e57e7dbf1fb5f5
mtail <mtail@google.com>
2019-07-31 12:49:38 -0700

    Address feedback.

--- docker/build-tools/build-and-push.sh
+++ docker/build-tools/build-and-push.sh
@@ -4,4 +4,4 @@ HUB=gcr.io/istio-testing
 VERSION=$(date +%Y-%m-%d)
 
 docker build --no-cache -t $HUB/build-tools:$VERSION .
-#docker push $HUB/build-tools:$VERSION
+docker push $HUB/build-tools:$VERSION
