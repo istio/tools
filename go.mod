6e49289d168d8eefa34b0fbf9f9ebbd54cc2431e
Jason Wang <jasonwzm@gmail.com>
2019-08-15 12:39:17 -0700

    Validate if CRD schema is structural during generation

--- go.mod
+++ go.mod
@@ -13,34 +13,28 @@ require (
 	github.com/gogo/protobuf v1.2.2-0.20190730201129-28a6bbf47e48
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/golang/protobuf v1.3.2
-	github.com/google/gofuzz v0.0.0-20170612174753-24818f796faf // indirect
-	github.com/google/uuid v0.0.0-20161128191214-064e2069ce9c
+	github.com/google/uuid v1.1.1
 	github.com/googleapis/gnostic v0.2.0 // indirect
 	github.com/gregjones/httpcache v0.0.0-20181110185634-c63ab54fda8f // indirect
 	github.com/hashicorp/go-multierror v1.0.0
 	github.com/imdario/mergo v0.3.6 // indirect
-	github.com/json-iterator/go v1.1.5 // indirect
 	github.com/jteeuwen/go-bindata v3.0.8-0.20180305030458-6025e8de665b+incompatible // indirect
 	github.com/kr/pretty v0.1.0
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
-	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
 	github.com/prometheus/client_golang v0.9.3
 	github.com/russross/blackfriday/v2 v2.0.1
 	github.com/shurcooL/httpfs v0.0.0-20190707220628-8d4bc4ba7749
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	github.com/shurcooL/vfsgen v0.0.0-20181202132449-6a9ea43bcacd
-	github.com/spf13/cobra v0.0.3
+	github.com/spf13/cobra v0.0.4
 	github.com/spf13/viper v1.4.0
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45 // indirect
-	golang.org/x/tools v0.0.0-20190328211700-ab21143f2384
+	golang.org/x/tools v0.0.0-20190614205625-5aca471b1d59
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.2.2
-	k8s.io/api v0.0.0-20190708094356-59223ed9f6ce
-	k8s.io/apiextensions-apiserver v0.0.0-20190708094842-2e94e9ac9840
-	k8s.io/apimachinery v0.0.0-20190221084156-01f179d85dbc
-	k8s.io/client-go v0.0.0-20190708094436-77c08c6b86df
-	k8s.io/gengo v0.0.0-20190128074634-0689ccc1d7d6
+	k8s.io/api v0.0.0-20190814101207-0772a1bdf941
+	k8s.io/apiextensions-apiserver v0.0.0-20190815061951-57d9ff808d8b
+	k8s.io/apimachinery v0.0.0-20190814100815-533d101be9a6
+	k8s.io/client-go v0.0.0-20190814101625-e290f153d148
+	k8s.io/gengo v0.0.0-20190813173942-955ffa8fcfc9
 	k8s.io/helm v2.12.0+incompatible
-	k8s.io/klog v0.1.0 // indirect
 )
--- openapi/cue/crd.go
+++ openapi/cue/crd.go
@@ -16,16 +16,19 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 
 	"cuelang.org/go/encoding/openapi"
 
-	apiext "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	apiext "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
+	apiextv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	structuralschema "k8s.io/apiextensions-apiserver/pkg/apiserver/schema"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // Interim solution to build the Istio CRDs before we move to KubeBuilder.
-func (x *builder) getCRD(crdCfg CrdConfig, schema interface{}) apiext.CustomResourceDefinition {
+func (x *builder) getCRD(crdCfg CrdConfig, schema interface{}) apiextv1beta1.CustomResourceDefinition {
 	// boilerplate OrderMap for CRD spec
 	m := &openapi.OrderedMap{}
 	m.Set("spec", schema)
@@ -47,15 +50,21 @@ func (x *builder) getCRD(crdCfg CrdConfig, schema interface{}) apiext.CustomReso
 	if err != nil {
 		log.Fatalf("Cannot marshal OpenAPI schema for %v", crdCfg.Metadata.Name)
 	}
-	j := &apiext.JSONSchemaProps{}
+	j := &apiextv1beta1.JSONSchemaProps{}
 	if err = json.Unmarshal(b, j); err != nil {
 		log.Fatalf("Cannot unmarshal raw OpenAPI schema to JSONSchemaProps for %v", crdCfg.Metadata.Name)
 	}
-	crdCfg.Spec.Validation = &apiext.CustomResourceValidation{OpenAPIV3Schema: j}
 
-	crd := apiext.CustomResourceDefinition{
+	fmt.Printf("Checking if the schema is structural for %v \n", crdCfg.Metadata.Name)
+	if err = validateStructural(j); err != nil {
+		log.Fatal(err)
+	}
+
+	crdCfg.Spec.Validation = &apiextv1beta1.CustomResourceValidation{OpenAPIV3Schema: j}
+
+	crd := apiextv1beta1.CustomResourceDefinition{
 		TypeMeta: metav1.TypeMeta{
-			APIVersion: apiext.SchemeGroupVersion.String(),
+			APIVersion: apiextv1beta1.SchemeGroupVersion.String(),
 			Kind:       "CustomResourceDefinition",
 		},
 		ObjectMeta: crdCfg.Metadata,
@@ -63,8 +72,26 @@ func (x *builder) getCRD(crdCfg CrdConfig, schema interface{}) apiext.CustomReso
 	}
 
 	// marshal to an empty field in the output
-	crd.Status.Conditions = []apiext.CustomResourceDefinitionCondition{}
+	crd.Status.Conditions = []apiextv1beta1.CustomResourceDefinitionCondition{}
 	crd.Status.StoredVersions = []string{}
 
 	return crd
+}
+
+func validateStructural(s *apiextv1beta1.JSONSchemaProps) error {
+	out := &apiext.JSONSchemaProps{}
+	if err := apiextv1beta1.Convert_v1beta1_JSONSchemaProps_To_apiextensions_JSONSchemaProps(s, out, nil); err != nil {
+		return fmt.Errorf("Cannot convert v1beta1 JSONSchemaProps to v1 JSONSchemaProps: %v", err)
+	}
+
+	r, err := structuralschema.NewStructural(out)
+	if err != nil {
+		return fmt.Errorf("Cannot convert to a structural schema: %v", err)
+	}
+
+	if errs := structuralschema.ValidateStructural(r, nil); len(errs) != 0 {
+		return fmt.Errorf("Schema is not structural: %v", errs.ToAggregate().Error())
+	}
+
+	return nil
 }
