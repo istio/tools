1435813688542ebc93cdcf05d9d2d288918d584a
Martin Taillefer <mtail@google.com>
2019-08-16 14:25:49 -0700

    Add support to build and publish container images from prow.

--- docker/build-tools/build-and-push.sh
+++ docker/build-tools/build-and-push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 HUB=gcr.io/istio-testing
-VERSION=$(date +%Y-%m-%d)
+VERSION=$(date +%Y-%m-%dT%H-%M-%S)
 
 docker build --no-cache -t $HUB/build-tools:$VERSION .
 docker push $HUB/build-tools:$VERSION
