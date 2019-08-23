3ef2c103fa33dd050f779d5f9adb735e4a9b36c1
carolynhu <carolynprh@gmail.com>
2019-08-22 17:05:26 -0700

    Fix Istio performance benchmarking prometheusOperator setup issue (#295)
    * Update perf/benchmark readme doc
    
    * Fix yaml file error, delet duplicate keys
    
    * Update setup_prometheus script to use the istio/installer

--- perf/istio-install/setup_prometheus.sh
+++ perf/istio-install/setup_prometheus.sh
@@ -8,38 +8,26 @@ mkdir -p "${WD}/tmp"
 function install_prometheus() {
   local DIRNAME="$1" # should be like tools/perf/istio/tmp
   DIRNAME=$(cd $DIRNAME; pwd)
-  local PROMETHEUS_INSTALL="${DIRNAME}"
+  INSTALLER_DIR="${DIRNAME}/installer"
+
+  git clone https://github.com/istio/installer.git  "$INSTALLER_DIR"
 
   # Create GCP SSD Storage class for Prometheus to use. May not work outside GKE
   kubectl apply -f "${WD}/../prometheus-install/ssd-storage-class.yaml"
 
-  helm fetch stable/prometheus-operator --untar --untardir "${PROMETHEUS_INSTALL}"
-
   kubectl create ns istio-prometheus || true
-  PROM_OP="${DIRNAME}prom-operator.yaml"
-  helm template "${PROMETHEUS_INSTALL}/prometheus-operator/" \
-    --namespace istio-prometheus \
-    --name prometheus \
-    -f "${WD}/../prometheus-install/values.yaml" > "${PROM_OP}"
+  kubectl label ns istio-prometheus istio-injection=disabled --overwrite
+  curl -s https://raw.githubusercontent.com/coreos/prometheus-operator/v0.31.1/bundle.yaml | sed "s/namespace: default/namespace: istio-prometheus/g" | kubectl apply -f -
+  kubectl -n istio-prometheus wait --for=condition=available --timeout=60s deploy/prometheus-operator
+  # kubectl wait is problematic, as the CRDs may not exist before the command is issued.
+  until timeout 60s kubectl get crds/prometheuses.monitoring.coreos.com; do echo "Waiting for CRDs to be created..."; done
+  until timeout 60s kubectl get crds/alertmanagers.monitoring.coreos.com; do echo "Waiting for CRDs to be created..."; done
+  until timeout 60s kubectl get crds/podmonitors.monitoring.coreos.com; do echo "Waiting for CRDs to be created..."; done
+  until timeout 60s kubectl get crds/prometheusrules.monitoring.coreos.com; do echo "Waiting for CRDs to be created..."; done
+  until timeout 60s kubectl get crds/servicemonitors.monitoring.coreos.com; do echo "Waiting for CRDs to be created..."; done
+  helm template --namespace istio-prometheus "${INSTALLER_DIR}"/istio-telemetry/prometheus-operator/ -f "${INSTALLER_DIR}"/global.yaml --set prometheus.createPrometheusResource=true | kubectl apply -n istio-prometheus -f -
 
-  echo "${PROM_OP}"
-
-  if [[ -z "${DRY_RUN}" ]]; then
-    kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/alertmanager.crd.yaml
-    kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/prometheus.crd.yaml
-    kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/prometheusrule.crd.yaml
-    kubectl apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/servicemonitor.crd.yaml
-
-    kubectl wait --for=condition=Established -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/alertmanager.crd.yaml
-    kubectl wait --for=condition=Established -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/prometheus.crd.yaml
-    kubectl wait --for=condition=Established -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/prometheusrule.crd.yaml
-    kubectl wait --for=condition=Established -f https://raw.githubusercontent.com/coreos/prometheus-operator/master/example/prometheus-operator-crd/servicemonitor.crd.yaml
-
-    kubectl apply --namespace istio-prometheus -f "${PROM_OP}"
-
-    # delete grafana pod so it redeploys with new config
-    kubectl delete pod -l app=grafana
-  fi
+  kubectl delete pod -l app=grafana
 }
 
 install_prometheus $1
