1128cb715d5474ed547dabc8f8106d88bef77147
Martin Taillefer <mtail@google.com>
2019-08-31 06:39:49 -0700

    Enable copyright banner linting.
    - Adds requisite copyright notice on many files.

--- setup/ipv6/setup_sn_ipv6k8s.sh
+++ setup/ipv6/setup_sn_ipv6k8s.sh
@@ -1,5 +1,19 @@
 #!/bin/bash -e
 
+# Copyright Istio Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #
 # Load common  waiting for control plane pods function
 #
@@ -65,7 +79,7 @@ wait_for_control_plane "${control_plane[@]}"
 
 kubectl get pods -n kube-system
 #
-# Installing calico networking 
+# Installing calico networking
 #
 kubectl create -f ./setup/ipv6/calico.yaml
 network_plane=("calico-node" "calico-kube-controllers")
