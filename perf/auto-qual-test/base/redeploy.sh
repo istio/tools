46cfeb7b78eff1738c10ff1f95775d3c6619de43
Martin Taillefer <geeknoid@users.noreply.github.com>
2019-08-31 10:33:04 -0700

    Enable copyright banner linting. (#317)
    - Adds requisite copyright notice on many files.

--- perf/auto-qual-test/base/redeploy.sh
+++ perf/auto-qual-test/base/redeploy.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+# Copyright Istio Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # this is a copy of istio.io/tools/bin/redeploy.sh because kustomize can't
 # handle references outside the current directory.
 function redeploy() {
@@ -26,7 +41,7 @@ function redeploy_all() {
 
 function main() {
   local ns=${1:?" specific namespace or ALL"}
-  
+
   if [[ "${ns}" == "ALL" ]];then
     redeploy_all
   else
--- perf/load/setup_large_test.sh
+++ perf/load/setup_large_test.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright Istio Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # This script spins up the standard 20 services per namespace test for as many namespaces
 # as desired.
 WD=$(dirname $0)
@@ -16,7 +30,7 @@ START=${2:-"0"}
 # service-graph04 svc04
 CMD=""
 if [[ ! -z "${ECHO}" ]];then
-  CMD="echo" 
+  CMD="echo"
 fi
 
 start_servicegraphs "${NUM}" "${START}"
