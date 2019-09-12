57c5e1c0e023a8f68b93c685917a6c1449ad3db5
Jianfei Hu <jianfeih@google.com>
2019-09-12 15:20:46 -0700

    a series of python scale.

--- perf/load/auto-mtls/scale.py
+++ perf/load/auto-mtls/scale.py
@@ -8,30 +8,51 @@ import subprocess
 import argparse
 
 
-def simulate_sidecar_rollout(sidecar, nosidecar: str):
+def get_deployment_replicas(namespace, deployment: str):
+  cmd = 'kubectl get deployment {dep} -n{ns} {jsonpath}'.format(
+    ns=namespace, dep=deployment, jsonpath='''-ojsonpath={.status.replicas}''')
+  p = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE)
+  return int(p.communicate()[0])
+
+
+def wait_deployment(namespace, deployment: str):
+  cmd = ('kubectl rollout status deployments/{dep} -n{ns}').format(
+    dep=deployment,
+    ns=namespace
+  )
+  print(cmd)
+  p = subprocess.Popen(cmd.split(' '))
+  p.wait()
+
+
+def scale_deployment(namespace, deployment: str, replica : int):
+  pass
+
+
+def simulate_sidecar_rollout(namespace, sidecar, nosidecar: str):
   '''
   Updates deployments with or without Envoy sidecar.
   wait indicates whether the command wait till all pods become ready.
   '''
-  # cmd = ('kubectl scale deployment.v1.apps/httpbin-sidecar --replicas={} -nauto-mtls').format(
-    # server_sidecar)
+  print('Namespace {}, sidecar deployment: {}, nosidecar deployment: {}'.format(
+      namespace, sidecar, nosidecar))
+  sidecar_count = get_deployment_replicas(namespace, sidecar)
+  nosidecar_count = get_deployment_replicas(namespace, nosidecar)
+  print('sidecar replica {}, nosidecar replica {}'.format(sidecar_count, nosidecar_count))
   while True:
-    print('Scale the deployment, sidecar deployment: %s, nosidecar deployment: %s', sidecar, nosidecar)
-    # cmd = ('kubectl get po -nauto-mtls')
-    # p = subprocess.Popen(cmd.split(' '))
-    # print(cmd)
-    # p.wait()
-    time.sleep(3)
-  # cmd = ('kubectl scale deployment.v1.apps/httpbin-nosidecar --replicas={} -nauto-mtls').format(
-    # server_nosidecar)
-  # p = subprocess.Popen(cmd.split(' '))
-  # p.wait()
+    wait_deployment(namespace, sidecar)
+    wait_deployment(namespace, nosidecar)
+    scale_deployment(namespace, sidecar, 0)
+    scale_deployment(namespace, nosidecar, 0)
+    time.sleep(3) # random amount of time.
+
 
 parser = argparse.ArgumentParser(description='Auto mTLS test runner')
 parser.add_argument('--sidecar-name', default='svc-0-automtls-sidecar', type=str)
 parser.add_argument('--nosidecar-name', default='svc-0-automtls-nosidecar', type=str)
+parser.add_argument('-n', '--namespace', default='auto-mtls', type=str)
 args = parser.parse_args()
 
 
 if __name__ == '__main__':
-  simulate_sidecar_rollout(args.sidecar_name, args.nosidecar_name)
+  simulate_sidecar_rollout(args.namespace, args.sidecar_name, args.nosidecar_name)
