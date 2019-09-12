54ef29244c54d1097eeaf492d4700fde981857d3
Jianfei Hu <jianfeih@google.com>
2019-09-12 14:32:02 -0700

    plumbing through two different deployments.

--- perf/load/auto-mtls/scale.py
+++ perf/load/auto-mtls/scale.py
@@ -8,7 +8,7 @@ import subprocess
 import argparse
 
 
-def simulate_sidecar_rollout(server_sidecar, server_nosidecar, wait=False):
+def simulate_sidecar_rollout(sidecar, nosidecar: str):
   '''
   Updates deployments with or without Envoy sidecar.
   wait indicates whether the command wait till all pods become ready.
@@ -16,17 +16,22 @@ def simulate_sidecar_rollout(server_sidecar, server_nosidecar, wait=False):
   # cmd = ('kubectl scale deployment.v1.apps/httpbin-sidecar --replicas={} -nauto-mtls').format(
     # server_sidecar)
   while True:
+    print('Scale the deployment, sidecar deployment: %s, nosidecar deployment: %s', sidecar, nosidecar)
     # cmd = ('kubectl get po -nauto-mtls')
     # p = subprocess.Popen(cmd.split(' '))
     # print(cmd)
     # p.wait()
-    print('jianfeih is debugging')
-    print("your message", file=sys.stderr)
     time.sleep(3)
   # cmd = ('kubectl scale deployment.v1.apps/httpbin-nosidecar --replicas={} -nauto-mtls').format(
     # server_nosidecar)
   # p = subprocess.Popen(cmd.split(' '))
   # p.wait()
 
+parser = argparse.ArgumentParser(description='Auto mTLS test runner')
+parser.add_argument('--sidecar-name', default='svc-0-automtls-sidecar', type=str)
+parser.add_argument('--nosidecar-name', default='svc-0-automtls-nosidecar', type=str)
+args = parser.parse_args()
+
+
 if __name__ == '__main__':
-  simulate_sidecar_rollout()
+  simulate_sidecar_rollout(args.sidecar_name, args.nosidecar_name)
