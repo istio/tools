72eb5b629bbdc2b44f28653250ea3d58a37ee94b
Jianfei Hu <jianfeih@google.com>
2019-09-13 13:58:47 -0700

    python format.

--- perf/load/auto-mtls/scale.py
+++ perf/load/auto-mtls/scale.py
@@ -30,65 +30,65 @@ ROLLOUT_INTERVAL_SEC = 5
 
 
 def get_deployment_replicas(namespace, deployment: str):
-  cmd = 'kubectl get deployment {dep} -n{ns} {jsonpath}'.format(
-    ns=namespace, dep=deployment, jsonpath='''-ojsonpath={.status.replicas}''')
-  p = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE)
-  output = p.communicate()[0]
-  if len(output) == 0:
-    return 0
-  return int(output)
+    cmd = 'kubectl get deployment {dep} -n{ns} {jsonpath}'.format(
+        ns=namespace, dep=deployment, jsonpath='''-ojsonpath={.status.replicas}''')
+    p = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE)
+    output = p.communicate()[0]
+    if len(output) == 0:
+        return 0
+    return int(output)
 
 
 def wait_deployment(namespace, deployment: str):
-  cmd = ('kubectl rollout status deployments/{dep} -n{ns}').format(
-    dep=deployment,
-    ns=namespace
-  )
-  print(cmd)
-  p = subprocess.Popen(cmd.split(' '))
-  p.wait()
+    cmd = ('kubectl rollout status deployments/{dep} -n{ns}').format(
+        dep=deployment,
+        ns=namespace
+    )
+    print(cmd)
+    p = subprocess.Popen(cmd.split(' '))
+    p.wait()
 
 
-def scale_deployment(namespace, deployment: str, replica : int):
-  cmd = 'kubectl scale deployment {dep} -n{ns} --replicas {replica}'.format(
-    dep=deployment, ns=namespace, replica=replica
-  )
-  print(cmd)
-  p = subprocess.Popen(cmd.split(' '))
-  p.wait()
+def scale_deployment(namespace, deployment: str, replica: int):
+    cmd = 'kubectl scale deployment {dep} -n{ns} --replicas {replica}'.format(
+        dep=deployment, ns=namespace, replica=replica
+    )
+    print(cmd)
+    p = subprocess.Popen(cmd.split(' '))
+    p.wait()
 
 
 def simulate_sidecar_rollout(namespace, sidecar_dep, nosidecar_dep: str):
-  '''
-  Updates deployments with or without Envoy sidecar.
-  wait indicates whether the command wait till all pods become ready.
-  '''
-  print('Namespace {}, sidecar deployment: {}, nosidecar deployment: {}'.format(
-      namespace, sidecar_dep, nosidecar_dep))
-  sidecar_count = get_deployment_replicas(namespace, sidecar_dep)
-  nosidecar_count = get_deployment_replicas(namespace, nosidecar_dep)
-  total = sidecar_count + nosidecar_count
-  print('sidecar replica {}, nosidecar replica {}'.format(sidecar_count, nosidecar_count))
-  iteration = 1
-  while True:
-    prob = random.random()
-    if prob < ALL_SIDECAR_PROB:
-      sidecar_count = total
-      nosidecar_count = 0
-    else:
-      sidecar_count = int(random.random() * total)
-      nosidecar_count = total - sidecar_count
-    print('======================================\n'
-      'Scale iteration {itr}, sidecar count {sc}, nosidecar count {nsc}\n\n'.format(
-      itr=iteration, sc=sidecar_count, nsc=nosidecar_count
-    ))
-    scale_deployment(namespace, sidecar_dep, sidecar_count)
-    scale_deployment(namespace, nosidecar_dep, nosidecar_count)
-    wait_deployment(namespace, sidecar_dep)
-    wait_deployment(namespace, nosidecar_dep)
-    iteration += 1
-    print('\n\n')
-    time.sleep(ROLLOUT_INTERVAL_SEC) # random amount of time.
+    '''
+    Updates deployments with or without Envoy sidecar.
+    wait indicates whether the command wait till all pods become ready.
+    '''
+    print('Namespace {}, sidecar deployment: {}, nosidecar deployment: {}'.format(
+        namespace, sidecar_dep, nosidecar_dep))
+    sidecar_count = get_deployment_replicas(namespace, sidecar_dep)
+    nosidecar_count = get_deployment_replicas(namespace, nosidecar_dep)
+    total = sidecar_count + nosidecar_count
+    print('sidecar replica {}, nosidecar replica {}'.format(sidecar_count, nosidecar_count))
+    iteration = 1
+    while True:
+        prob = random.random()
+        if prob < ALL_SIDECAR_PROB:
+            sidecar_count = total
+            nosidecar_count = 0
+        else:
+            sidecar_count = int(random.random() * total)
+            nosidecar_count = total - sidecar_count
+        print('======================================\n'
+              'Scale iteration {itr}, sidecar count {sc}, nosidecar count {nsc}\n\n'.format(
+                  itr=iteration, sc=sidecar_count, nsc=nosidecar_count
+              ))
+        scale_deployment(namespace, sidecar_dep, sidecar_count)
+        scale_deployment(namespace, nosidecar_dep, nosidecar_count)
+        wait_deployment(namespace, sidecar_dep)
+        wait_deployment(namespace, nosidecar_dep)
+        iteration += 1
+        print('\n\n')
+        time.sleep(ROLLOUT_INTERVAL_SEC)  # random amount of time.
 
 
 parser = argparse.ArgumentParser(description='Auto mTLS test runner')
@@ -99,4 +99,4 @@ args = parser.parse_args()
 
 
 if __name__ == '__main__':
-  simulate_sidecar_rollout(args.namespace, args.sidecar_name, args.nosidecar_name)
+    simulate_sidecar_rollout(args.namespace, args.sidecar_name, args.nosidecar_name)
