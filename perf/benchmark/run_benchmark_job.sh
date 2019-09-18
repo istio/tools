5a08928029ddb9fca2b357d303b0df0b9e93c111
Xinnan Wen <iamwen@google.com>
2019-09-17 22:26:50 -0700

    Fix lint

--- perf/benchmark/run_benchmark_job.sh
+++ perf/benchmark/run_benchmark_job.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Copyright Istio Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#!/bin/bash
 WD=$(dirname "$0")
 WD=$(cd "$WD"; pwd)
 ROOT=$(dirname "$WD")
