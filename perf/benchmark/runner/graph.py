f7129b0086b6e444296d860b1b5be5ca7316d6ca
Johannes M. Scheuermann <joh.scheuer@gmail.com>
2019-12-03 08:41:38 +0100

    Correct graph script to print mixer values

--- perf/benchmark/runner/graph.py
+++ perf/benchmark/runner/graph.py
@@ -93,7 +93,7 @@ def get_series(df, x_label, metric):
              ("nomix.*_both", "nomixer_both"),
              ("base", "base"),
              # Make sure this set of data is last, because both type always have data to make sure rows are not empty.
-             ("^both", "both")]
+             (".*mixer_both", "mixer_both")]
 
     # get y axis
     series = {}
