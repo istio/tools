1d95139450efd9d6cd326ec269fd95258060fb58
John Howard <howardjohn@google.com>
2019-12-04 10:04:27 -0800

    Upgrade html proofer

--- docker/build-tools/build-and-push.sh
+++ docker/build-tools/build-and-push.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -x
+
 # support other container tools, e.g. podman
 CONTAINER_CLI=${CONTAINER_CLI:-docker}
 
