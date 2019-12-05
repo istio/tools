66e8bec3a57c3f3bd94869288f54329855e00a3e
carolynhu <carolynprh@gmail.com>
2019-12-05 11:48:01 -0800

    fix lint error

--- perf_dashboard/perf_dashboard/settings.py
+++ perf_dashboard/perf_dashboard/settings.py
@@ -73,8 +73,7 @@ ROOT_URLCONF = 'perf_dashboard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
