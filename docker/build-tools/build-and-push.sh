e888256e82dad860dfd5428632781d6776796d69
John Howard <howardjohn@google.com>
2019-12-09 08:10:46 -0800

    eux

--- docker/build-tools/build-and-push.sh
+++ docker/build-tools/build-and-push.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -x
+set -eux
 
 # support other container tools, e.g. podman
 CONTAINER_CLI=${CONTAINER_CLI:-docker}
@@ -28,7 +28,7 @@ SHA=$(git rev-parse ${BRANCH})
 ${CONTAINER_CLI} build --target build_tools --build-arg "ISTIO_TOOLS_SHA=${SHA}" -t "${HUB}/build-tools:${VERSION}" -t "${HUB}/build-tools:${BRANCH}-latest" .
 ${CONTAINER_CLI} build --build-arg "ISTIO_TOOLS_SHA=${SHA}" -t "${HUB}/build-tools-proxy:${VERSION}" -t "${HUB}/build-tools-proxy:${BRANCH}-latest" .
 
-if [[ -z "${DRY_RUN}" ]]; then
+if [[ -z "${DRY_RUN:-}" ]]; then
   ${CONTAINER_CLI} push "${HUB}/build-tools:${VERSION}"
   ${CONTAINER_CLI} push "${HUB}/build-tools:${BRANCH}-latest"
   ${CONTAINER_CLI} push "${HUB}/build-tools-proxy:${VERSION}"
