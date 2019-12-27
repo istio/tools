df40bf9e9469bde81009f3a1e9def9740b3c0b99
Jianfei Hu <jianfeih@google.com>
2019-12-27 09:53:20 -0800

    Plain text profile as two pods benchmark testing. (#653)
    * initial version of plaintext setup.
    
    * clean up readme and mode running log printf
    
    * runner update both mode wording
    
    * finish the test mode.
    
    * update print msg.
    
    * lint fix.
    
    * clientsiecar rename.

--- perf/benchmark/runner/runner.py
+++ perf/benchmark/runner/runner.py
@@ -85,7 +85,7 @@ class Fortio:
             extra_labels=None,
             baseline=False,
             serversidecar=False,
-            clientsidecar=True,
+            bothsidecar=True,
             ingress=None,
             mesh="istio"):
         self.run_id = str(uuid.uuid4()).partition('-')[0]
@@ -107,7 +107,7 @@ class Fortio:
         self.extra_labels = extra_labels
         self.run_baseline = baseline
         self.run_serversidecar = serversidecar
-        self.run_clientsidecar = clientsidecar
+        self.run_bothsidecar = bothsidecar
         self.run_ingress = ingress
 
         if mesh == "linkerd":
@@ -181,6 +181,7 @@ class Fortio:
                 labels=labels)
 
         if self.run_ingress:
+            print('--------------Running in ingress mode--------------')
             kubectl_exec(self.client.name, self.ingress(fortio_cmd))
             if self.perf_record:
                 run_perf(
@@ -190,6 +191,7 @@ class Fortio:
                     duration=40)
 
         if self.run_serversidecar:
+            print('--------------Running in server sidecar mode--------------')
             kubectl_exec(self.client.name, self.serversidecar(fortio_cmd))
             if self.perf_record:
                 run_perf(
@@ -198,7 +200,8 @@ class Fortio:
                     labels + "_srv_serveronly",
                     duration=40)
 
-        if self.run_clientsidecar:
+        if self.run_bothsidecar:
+            print('--------------Running in both sidecar mode--------------')
             kubectl_exec(self.client.name, self.bothsidecar(fortio_cmd))
             if self.perf_record:
                 run_perf(
@@ -208,6 +211,7 @@ class Fortio:
                     duration=40)
 
         if self.run_baseline:
+            print('--------------Running in baseline mode--------------')
             kubectl_exec(self.client.name, self.nosidecar(fortio_cmd))
 
 
@@ -307,6 +311,7 @@ def fortio_from_config_file(args):
         fortio.size = job_config.get('size', 1024)
         fortio.perf_record = job_config.get('perf_record', False)
         fortio.run_serversidecar = job_config.get('run_serversidecar', False)
+        fortio.run_bothsidecar = job_config.get('run_bothsidecar', False)
         fortio.run_baseline = job_config.get('run_baseline', True)
         fortio.mesh = job_config.get('mesh', 'istio')
         fortio.mode = job_config.get('mode', 'http')
@@ -330,7 +335,7 @@ def run(args):
             extra_labels=args.extra_labels,
             baseline=args.baseline,
             serversidecar=args.serversidecar,
-            clientsidecar=args.clientsidecar,
+            bothsidecar=args.bothsidecar,
             ingress=args.ingress,
             mode=args.mode,
             mesh=args.mesh,
@@ -411,8 +416,8 @@ def get_parser():
     define_bool(parser, "baseline", "run baseline for all", False)
     define_bool(parser, "serversidecar",
                 "run serversidecar-only for all", False)
-    define_bool(parser, "clientsidecar",
-                "run clientsidecar and serversidecar for all", True)
+    define_bool(parser, "bothsidecar",
+                "run clientsiecar and serversidecar for all", True)
 
     return parser
 
