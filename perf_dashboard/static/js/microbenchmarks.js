52995263756333353cee6c2be00197a43ac2d185
carolynhu <carolynprh@gmail.com>
2020-01-09 11:25:45 -0800

    rename assets to static

---
+++ perf_dashboard/static/js/microbenchmarks.js
@@ -0,0 +1,181 @@
+
+// ------------------------------------------------------- //
+// Charts Gradients
+// ------------------------------------------------------ //
+var ctx1 = $("canvas").get(0).getContext("2d");
+var gradient1 = ctx1.createLinearGradient(150, 0, 150, 300);
+gradient1.addColorStop(0, 'rgba(133, 180, 242, 0.91)');
+gradient1.addColorStop(1, 'rgba(255, 119, 119, 0.94)');
+
+var gradient2 = ctx1.createLinearGradient(146.000, 0.000, 154.000, 300.000);
+gradient2.addColorStop(0, 'rgba(104, 179, 112, 0.85)');
+gradient2.addColorStop(1, 'rgba(76, 162, 205, 0.85)');
+
+
+    // ------------------------------------------------------- //
+    // Line Chart
+    // ------------------------------------------------------ //
+
+new Chart(document.getElementById("lineChartExample"), {
+        type: 'line',
+        options: {
+            legend: {labels:{fontColor:"#777", fontSize: 12}},
+            scales: {
+                xAxes: [{
+                    display: true,
+                    gridLines: {
+                        color: '#eee'
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    gridLines: {
+                        color: '#eee'
+                    }
+                }]
+            },
+        },
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "Data Set One",
+                    fill: true,
+                    lineTension: 0.3,
+                    backgroundColor: gradient1,
+                    borderColor: gradient1,
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    borderWidth: 1,
+                    pointBorderColor: gradient1,
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: gradient1,
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [30, 50, 40, 61, 42, 35, 40],
+                    spanGaps: false
+                },
+                {
+                    label: "Data Set Two",
+                    fill: true,
+                    lineTension: 0.3,
+                    backgroundColor: gradient2,
+                    borderColor: gradient2,
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    borderWidth: 1,
+                    pointBorderColor: gradient2,
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: gradient2,
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [50, 40, 50, 40, 45, 40, 30],
+                    spanGaps: false
+                }
+            ]
+        }
+});
+
+    // ------------------------------------------------------- //
+    // Bar Chart
+    // ------------------------------------------------------ //
+new Chart(document.getElementById("barChartExample"), {
+        type: 'bar',
+        options: {
+            scales: {
+                xAxes: [{
+                    display: true,
+                    gridLines: {
+                        color: '#eee'
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    gridLines: {
+                        color: '#eee'
+                    }
+                }]
+            },
+        },
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "Data Set 1",
+                    backgroundColor: [
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1
+                    ],
+                    hoverBackgroundColor: [
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1
+                    ],
+                    borderColor: [
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1,
+                        gradient1
+                    ],
+                    borderWidth: 1,
+                    data: [65, 59, 80, 81, 56, 55, 40],
+                },
+                {
+                    label: "Data Set 2",
+                    backgroundColor: [
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2
+                    ],
+                    hoverBackgroundColor: [
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2
+                    ],
+                    borderColor: [
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2,
+                        gradient2
+                    ],
+                    borderWidth: 1,
+                    data: [35, 40, 60, 47, 88, 27, 30],
+                }
+            ]
+        }
+});
