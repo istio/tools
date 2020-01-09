97a544597f848900dd5d5fa456c0665442e20b2a
carolynhu <carolynprh@gmail.com>
2020-01-09 11:39:01 -0800

    Rename staticfiles folder and Add essential docker related files for deploy perfboard to K8S (#666)
    * delete assets
    
    * rename assets to static
    
    * update settings.py file
    
    * add docker-entrypoint bash
    
    * update dockerfile
    
    * add missing license and copyright to fix linting error
    
    * resolve comments

--- perf_dashboard/perf_dashboard/settings.py
+++ perf_dashboard/perf_dashboard/settings.py
@@ -37,9 +37,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0-k61x@nbmohu87^f!gql9ug1c*5(8k@ujeeme%cx4x=d@*=-f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'perf.dashboard.qualistio.org']
 
 
 # Application definition
@@ -133,8 +133,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets'),
-)
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
