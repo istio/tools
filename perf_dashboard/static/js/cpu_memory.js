3ab4a999f64d7cef2f9207df5370a3089e901572
carolynhu <carolynprh@gmail.com>
2020-02-03 10:45:08 -0800

    Add graph plotting page for rendering user uploaded testing data (#678)
    * add graph plotting page
    
    * fix linter error
    
    * remove unnecessary file
    
    * pass value from template to view
    
    * do some clean up
    
    * add more options for latency
    
    * add some initial working logic
    
    * finish all functionality thing
    
    * delete uploaded file
    
    * rename back
    
    * fix linter error
    
    * specify the csv benchmark link
    
    * fix linting error

--- perf_dashboard/static/js/cpu_memory.js
+++ perf_dashboard/static/js/cpu_memory.js
@@ -94,65 +94,6 @@ new Chart(document.getElementById("cpu-qps-release"), {
     options: cpuOptions
 });
 
-new Chart(document.getElementById("cpu-qps-master"), {
-    type: 'line',
-    data: {
-        labels: qpsNum,
-        datasets: [
-            {
-                label: "baseline",
-                backgroundColor: "rgba(236, 66, 53, 0.2)",
-                borderColor: "rgba(236, 66, 53, 1)",
-                data: cpu_mixer_base_master,
-                fill: false
-            }, {
-                label: "serveronly",
-                backgroundColor: "rgba(259, 188, 5, 0.2)",
-                borderColor: "rgba(259, 188, 5, 1)",
-                data: cpu_mixer_serveronly_master,
-                fill: false
-            }, {
-                label: "both",
-                backgroundColor: "rgba(66, 133, 246, 0.2)",
-                borderColor: "rgba(66, 133, 246, 1)",
-                data: cpu_mixer_both_master,
-                fill: false
-            }, {
-                label: "none-serveronly",
-                backgroundColor: "rgba(52, 168, 85, 0.2)",
-                borderColor: "rgba(52, 168, 85, 1)",
-                data: cpu_none_serveronly_master,
-                fill: false
-            }, {
-                label: "none-both",
-                backgroundColor: "rgba(0, 0, 0, 0.2)",
-                borderColor: "rgba(0, 0, 0, 1)",
-                data: cpu_none_both_master,
-                fill: false
-            }, {
-                label: "none-plaintext-both",
-                backgroundColor: "rgba(52, 235, 219, 0.2)",
-                borderColor: "rgba(52, 235, 219, 1)",
-                data: cpu_none_plaintext_both_master,
-                fill: false
-            }, {
-                label: "v2-serveronly",
-                backgroundColor: "rgba(168, 50, 168, 0.2)",
-                borderColor: "rgba(168, 50, 168, 1)",
-                data: cpu_v2_serveronly_master,
-                fill: false
-            }, {
-                label: "v2-both",
-                backgroundColor: "rgba(252, 123, 3, 0.2)",
-                borderColor: "rgba(252, 123, 3, 1)",
-                data: cpu_v2_both_master,
-                fill: false
-            }
-        ]
-    },
-    options: cpuOptions
-});
-
 new Chart(document.getElementById("mem-qps-release"), {
     type: 'line',
     data: {
@@ -210,6 +151,65 @@ new Chart(document.getElementById("mem-qps-release"), {
         ]
     },
     options: memOptions
+});
+
+new Chart(document.getElementById("cpu-qps-master"), {
+    type: 'line',
+    data: {
+        labels: qpsNum,
+        datasets: [
+            {
+                label: "baseline",
+                backgroundColor: "rgba(236, 66, 53, 0.2)",
+                borderColor: "rgba(236, 66, 53, 1)",
+                data: cpu_mixer_base_master,
+                fill: false
+            }, {
+                label: "serveronly",
+                backgroundColor: "rgba(259, 188, 5, 0.2)",
+                borderColor: "rgba(259, 188, 5, 1)",
+                data: cpu_mixer_serveronly_master,
+                fill: false
+            }, {
+                label: "both",
+                backgroundColor: "rgba(66, 133, 246, 0.2)",
+                borderColor: "rgba(66, 133, 246, 1)",
+                data: cpu_mixer_both_master,
+                fill: false
+            }, {
+                label: "none-serveronly",
+                backgroundColor: "rgba(52, 168, 85, 0.2)",
+                borderColor: "rgba(52, 168, 85, 1)",
+                data: cpu_none_serveronly_master,
+                fill: false
+            }, {
+                label: "none-both",
+                backgroundColor: "rgba(0, 0, 0, 0.2)",
+                borderColor: "rgba(0, 0, 0, 1)",
+                data: cpu_none_both_master,
+                fill: false
+            }, {
+                label: "none-plaintext-both",
+                backgroundColor: "rgba(52, 235, 219, 0.2)",
+                borderColor: "rgba(52, 235, 219, 1)",
+                data: cpu_none_plaintext_both_master,
+                fill: false
+            }, {
+                label: "v2-serveronly",
+                backgroundColor: "rgba(168, 50, 168, 0.2)",
+                borderColor: "rgba(168, 50, 168, 1)",
+                data: cpu_v2_serveronly_master,
+                fill: false
+            }, {
+                label: "v2-both",
+                backgroundColor: "rgba(252, 123, 3, 0.2)",
+                borderColor: "rgba(252, 123, 3, 1)",
+                data: cpu_v2_both_master,
+                fill: false
+            }
+        ]
+    },
+    options: cpuOptions
 });
 
 new Chart(document.getElementById("mem-qps-master"), {
