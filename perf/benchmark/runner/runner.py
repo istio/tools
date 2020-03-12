eeccf5873ac249eafd746508356765ec57063ed6
Istio Automation <istio.testing@gmail.com>
2020-03-12 12:38:13 -0700

    fix headers keyerror issue in fortio cmd (#806)
    Co-authored-by: carolynhu <carolynprh@google.com>

--- perf/benchmark/runner/runner.py
+++ perf/benchmark/runner/runner.py
@@ -190,9 +190,9 @@ class Fortio:
                 headers_cmd += "-H=" + header_val + " "
 
         fortio_cmd = (
-            "fortio load {headers_cmd} -c {conn} -qps {qps} -t {duration}s -a -r {r} {cacert_arg} {grpc} -httpbufferkb=128 " +
+            "fortio load {headers} -c {conn} -qps {qps} -t {duration}s -a -r {r} {cacert_arg} {grpc} -httpbufferkb=128 " +
             "-labels {labels}").format(
-                headers=headers,
+                headers=headers_cmd,
                 conn=conn,
                 qps=qps,
                 duration=duration,
