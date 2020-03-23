976f5fa267c061251be50874b918f3f564ac436d
carolynhu <carolynprh@google.com>
2020-03-23 10:22:29 -0700

    disable clientonly and serveronly modes in displaying on perf dashboard (#818)

--- perf_dashboard/benchmarks/views.py
+++ perf_dashboard/benchmarks/views.py
@@ -579,6 +579,8 @@ def micro_benchmarks(request):
 # Helpers
 def get_latency_vs_conn_y_series(df, mixer_mode, quantiles):
     y_series_data = []
+    if ("serveronly" in mixer_mode) or ("clientonly" in mixer_mode):
+        return []
     for thread in [2, 4, 8, 16, 32, 64]:
         data = df.query('ActualQPS == 1000 and NumThreads == @thread and Labels.str.endswith(@mixer_mode)')
         if not data[quantiles].head().empty:
@@ -590,6 +592,8 @@ def get_latency_vs_conn_y_series(df, mixer_mode, quantiles):
 
 def get_latency_vs_qps_y_series(df, mixer_mode, quantiles):
     y_series_data = []
+    if ("serveronly" in mixer_mode) or ("clientonly" in mixer_mode):
+        return []
     for qps in [10, 100, 500, 1000, 2000, 3000]:
         data = df.query('ActualQPS == @qps and NumThreads == 16 and Labels.str.endswith(@mixer_mode)')
         if not data[quantiles].head().empty:
@@ -601,6 +605,8 @@ def get_latency_vs_qps_y_series(df, mixer_mode, quantiles):
 
 def get_cpu_y_series(df, mixer_mode):
     y_series_data = []
+    if ("serveronly" in mixer_mode) or ("clientonly" in mixer_mode):
+        return []
     cpu_metric = 'cpu_mili_avg_fortioserver_deployment_proxy'
     for qps in [10, 100, 500, 1000, 2000, 3000]:
         data = df.query('ActualQPS == @qps and NumThreads == 16 and Labels.str.endswith(@mixer_mode)')
@@ -613,6 +619,8 @@ def get_cpu_y_series(df, mixer_mode):
 
 def get_mem_y_series(df, mixer_mode):
     y_series_data = []
+    if ("serveronly" in mixer_mode) or ("clientonly" in mixer_mode):
+        return []
     mem_metric = 'mem_MB_max_fortioserver_deployment_proxy'
     for qps in [10, 100, 500, 1000, 2000, 3000]:
         data = df.query('ActualQPS == @qps and NumThreads == 16 and Labels.str.endswith(@mixer_mode)')
