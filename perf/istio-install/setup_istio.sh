617b4ec751c443db1f154de1466133c4a42ffbf7
carolynhu <carolynprh@google.com>
2020-04-16 10:01:10 -0700

    fix Asset charts not found when installing istio

--- perf/istio-install/setup_istio.sh
+++ perf/istio-install/setup_istio.sh
@@ -142,7 +142,7 @@ function install_istio_with_helm() {
 function install_istio_with_istioctl() {
   local CR_PATH="${WD}/istioctl_profiles/${CR_FILENAME}"
   pushd "${ISTIOCTL_PATH}"
-  ./istioctl manifest apply -f "${CR_PATH}" --set "${SET_OVERLAY}" "${EXTRA_ARGS}"
+  ./istioctl --charts manifest apply -f "${CR_PATH}" --set "${SET_OVERLAY}" "${EXTRA_ARGS}"
   popd
 }
 
