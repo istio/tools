1e608b6f15441b66d202aa0a28771ee47c53ff76
carolynhu <carolynprh@google.com>
2020-05-08 14:41:14 -0700

    Make perf test run with clear setup/run steps and some cleanups (#943)
    * Make perf test run with clear setup/run steps and some cleanups
    
    * fix lint error
    
    * enable telemetryv2 statswasm run
    
    * delete collect_metrics part
    
    * TAG should be INSTALL_VERSION
    
    * remove GIT_SHA
    
    * add collect_metrics back and revert fortio.py file change
    
    * add report_port logic for handle both fortio and nighthawk

--- perf/benchmark/run_benchmark_job.sh
+++ perf/benchmark/run_benchmark_job.sh
@@ -25,41 +25,134 @@ set -u
 # Print commands
 set -x
 
-# This is config for postsubmit cluster.
-export VALUES="${VALUES:-values-istio-postsubmit.yaml}"
-export DNS_DOMAIN="fake-dns.org"
-export FORTIO_CLIENT_URL=""
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-export GCS_BUCKET="istio-build/perf"
+
+# Boskos cluster related Env vars
+# This is the config for postsubmit cluster.
+export VALUES="${VALUES:-values-istio-postsubmit.yaml}"
 # Check https://github.com/istio/test-infra/blob/master/boskos/configs.yaml
 # for existing resources types
 export RESOURCE_TYPE="${RESOURCE_TYPE:-gke-perf-preset}"
-export OWNER="${OWNER:-perf-tests}"
 export PILOT_CLUSTER="${PILOT_CLUSTER:-}"
 export USE_MASON_RESOURCE="${USE_MASON_RESOURCE:-True}"
 export CLEAN_CLUSTERS="${CLEAN_CLUSTERS:-True}"
+export OWNER="${OWNER:-perf-tests}"
+
+# Istio performance test related Env vars
 export NAMESPACE=${NAMESPACE:-'twopods-istio'}
 export PROMETHEUS_NAMESPACE=${PROMETHEUS_NAMESPACE:-'istio-system'}
-export TRIALRUN=${TRIALRUN:-"False"}
+export ISTIO_INJECT=${ISTIO_INJECT:-true}
+export DNS_DOMAIN="fake-dns.org"
 export LOAD_GEN_TYPE=${LOAD_GEN_TYPE:-"fortio"}
+export FORTIO_CLIENT_URL=""
+
+# Other Env vars
+export GCS_BUCKET="istio-build/perf"
+export TRIALRUN=${TRIALRUN:-"False"}
+
 
 CLEANUP_PIDS=()
 
-function setup_metrics() {
-  # shellcheck disable=SC2155
-  INGRESS_IP="$(kubectl get services -n "${NAMESPACE}" fortioclient -o jsonpath="{.status.loadBalancer.ingress[0].ip}")"
-  export FORTIO_CLIENT_URL=http://${INGRESS_IP}:8080
-  if [[ -z "$INGRESS_IP" ]];then
-    kubectl -n "${NAMESPACE}" port-forward svc/fortioclient 8080:8080 &
+# Step 1: setup cluster
+helm init --client-only
+# shellcheck disable=SC1090
+source "${ROOT}/../bin/setup_cluster.sh"
+setup_e2e_cluster
+
+# Step 2: install Istio
+# Setup release info
+BRANCH="latest"
+if [ "${GIT_BRANCH}" != "master" ];then
+  BRANCH_NUM=$(echo "$GIT_BRANCH" | cut -f2 -d-)
+  BRANCH="${BRANCH_NUM}-dev"
+fi
+
+# Different branch tag resides in dev release directory like /latest, /1.4-dev, /1.5-dev etc.
+INSTALL_VERSION=$(curl "https://storage.googleapis.com/istio-build/dev/${BRANCH}")
+echo "Setup istio release: ${INSTALL_VERSION}"
+
+pushd "${ROOT}/istio-install"
+   DEV_VERSION=${INSTALL_VERSION} ./setup_istio.sh -f istioctl_profiles/default.yaml
+popd
+
+# Step 3: setup Istio performance test
+pushd "${WD}"
+export ISTIO_INJECT="true"
+./setup_test.sh
+popd
+
+# Step 4: install Python dependencies
+# Install pipenv
+if [[ $(command -v pipenv) == "" ]];then
+  apt-get update && apt-get -y install python3-pip
+  pip3 install pipenv
+fi
+
+# Install dependencies
+cd "${WD}/runner"
+pipenv install
+
+# Step 5: setup perf data local output directory
+dt=$(date +'%Y%m%d')
+# Current output dir should be like: 20191025_1.5-alpha.f19fb40b777e357b605e85c04fb871578592ad1e
+export OUTPUT_DIR="${dt}_${INSTALL_VERSION}"
+LOCAL_OUTPUT_DIR="/tmp/${OUTPUT_DIR}"
+mkdir -p "${LOCAL_OUTPUT_DIR}"
+
+# Step 6: setup fortio and prometheus
+function setup_fortio_and_prometheus() {
+    # shellcheck disable=SC2155
+    INGRESS_IP="$(kubectl get services -n "${NAMESPACE}" fortioclient -o jsonpath="{.status.loadBalancer.ingress[0].ip}")"
+    local report_port="8080"
+    if [[ "${LOAD_GEN_TYPE}" == "nighthawk" ]]; then
+        report_port="9076"
+    fi
+
+    export FORTIO_CLIENT_URL=http://${INGRESS_IP}:${report_port}
+    if [[ -z "$INGRESS_IP" ]];then
+        kubectl -n "${NAMESPACE}" port-forward svc/fortioclient ${report_port}:${report_port} &
+        CLEANUP_PIDS+=("$!")
+        export FORTIO_CLIENT_URL=http://localhost:${report_port}
+    fi
+
+    export PROMETHEUS_URL=http://localhost:9090
+    kubectl -n "${PROMETHEUS_NAMESPACE}" port-forward svc/prometheus 9090:9090 &>/dev/null &
     CLEANUP_PIDS+=("$!")
-    export FORTIO_CLIENT_URL=http://localhost:8080
-  fi
-  export PROMETHEUS_URL=http://localhost:9090
-  kubectl -n "${PROMETHEUS_NAMESPACE}" port-forward svc/prometheus 9090:9090 &>/dev/null &
-  CLEANUP_PIDS+=("$!")
+
+    FORTIO_CLIENT_POD=$(kubectl get pods -n "${NAMESPACE}" | grep fortioclient | awk '{print $1}')
+    export FORTIO_CLIENT_POD
+    FORTIO_SERVER_POD=$(kubectl get pods -n "${NAMESPACE}" | grep fortioserver | awk '{print $1}')
+    export FORTIO_SERVER_POD
 }
 
+setup_fortio_and_prometheus
+
+# Step 7: setup exit handling
+function exit_handling() {
+  for pid in "${CLEANUP_PIDS[@]}"; do
+    kill "${pid}"
+  done
+
+  if [[ "${TRIALRUN}" == "True" ]]; then
+     exit 0
+  fi
+
+  # Copy raw data from fortio client pod
+  kubectl --namespace "${NAMESPACE}" cp "${FORTIO_CLIENT_POD}":/var/lib/fortio /tmp/rawdata -c shell
+  gsutil -q cp -r /tmp/rawdata "gs://${GCS_BUCKET}/${OUTPUT_DIR}/rawdata"
+  # output information for debugging
+  kubectl logs -n "${NAMESPACE}" "${FORTIO_CLIENT_POD}" -c captured || true
+  kubectl top pods --containers -n "${NAMESPACE}" || true
+  kubectl describe pods "${FORTIO_CLIENT_POD}" -n "${NAMESPACE}" || true
+}
+
+# add trap to copy raw data when exiting, also output logging information for debugging
+trap exit_handling ERR
+trap exit_handling EXIT
+
+# Step 8: run Istio performance test
+# Helper functions
 function collect_metrics() {
   # shellcheck disable=SC2155
   export CSV_OUTPUT="$(mktemp /tmp/benchmark_XXXX.csv)"
@@ -71,60 +164,27 @@ mem_MB_max_fortioserver_deployment_proxy,cpu_mili_avg_ingressgateway_proxy,cpu_m
   gsutil -q cp "${CSV_OUTPUT}" "gs://${GCS_BUCKET}/${OUTPUT_DIR}/benchmark.csv"
 }
 
-function collect_flame_graph() {
-    FLAME_OUTPUT_DIR="${WD}/flame/flameoutput/"
-    gsutil -q cp -r "${FLAME_OUTPUT_DIR}" "gs://${GCS_BUCKET}/${OUTPUT_DIR}/flamegraphs"
-}
-
-function generate_graph() {
-  local PLOT_METRIC=$1
-  pipenv run python3 graph.py "${CSV_OUTPUT}" "${PLOT_METRIC}" --charts_output_dir="${LOCAL_OUTPUT_DIR}"
-}
-
-function get_benchmark_data() {
+function run_benchmark_test() {
   pushd "${WD}/runner"
   CONFIG_FILE="${1}"
   pipenv run python3 runner.py --config_file "${CONFIG_FILE}"
-  
+
   if [[ "${TRIALRUN}" == "False" ]]; then
     collect_metrics
   fi
-
   popd
 }
 
-function exit_handling() {
-  for pid in "${CLEANUP_PIDS[@]}"; do
-    kill "${pid}"
-  done
-  if [[ "${TRIALRUN}" == "True" ]]; then
-     exit 0
-  fi
-  # copy raw data from fortio client pod
-  kubectl --namespace "${NAMESPACE}" cp "${FORTIO_CLIENT_POD}":/var/lib/fortio /tmp/rawdata -c shell
-  gsutil -q cp -r /tmp/rawdata "gs://${GCS_BUCKET}/${OUTPUT_DIR}/rawdata"
-  # output information for debugging
-  kubectl logs -n "${NAMESPACE}" "${FORTIO_CLIENT_POD}" -c captured || true
-  kubectl top pods --containers -n "${NAMESPACE}" || true
-  kubectl describe pods "${FORTIO_CLIENT_POD}" -n "${NAMESPACE}" || true
-}
-
-function enable_perf_record() {
-  nodes=$(kubectl get nodes -o=jsonpath='{.items[*].metadata.name}')
-
-  for node in $nodes
-  do
-    gcloud compute ssh --command "sudo sysctl kernel.perf_event_paranoid=-1;sudo sysctl kernel.kptr_restrict=0;exit" \
-    --zone us-central1-f bootstrap@"$node"
-  done
-}
-
-function setup_fortio_and_prometheus() {
-    setup_metrics
-    FORTIO_CLIENT_POD=$(kubectl get pods -n "${NAMESPACE}" | grep fortioclient | awk '{print $1}')
-    export FORTIO_CLIENT_POD
-    FORTIO_SERVER_POD=$(kubectl get pods -n "${NAMESPACE}" | grep fortioserver | awk '{print $1}')
-    export FORTIO_SERVER_POD
+function read_perf_test_conf() {
+  perf_test_conf="${1}"
+  while IFS="=" read -r key value; do
+    case "$key" in
+      '#'*) ;;
+      *)
+        # shellcheck disable=SC2086
+        export ${key}="${value}"
+    esac
+  done < "${perf_test_conf}"
 }
 
 function collect_envoy_info() {
@@ -147,90 +207,25 @@ function collect_clusters_info() {
   collect_envoy_info "${1}" "${FORTIO_SERVER_POD}" "clusters"
 }
 
-function read_perf_test_conf()
-{
-  perf_test_conf="${1}"
-  while IFS="=" read -r key value; do
-    case "$key" in
-      '#'*) ;;
-      *)
-        # shellcheck disable=SC2086
-        export ${key}="${value}"
-    esac
-  done < "${perf_test_conf}"
-}
-
-# install pipenv
-if [[ $(command -v pipenv) == "" ]];then
-  apt-get update && apt-get -y install python3-pip
-  pip3 install pipenv
-fi
-
-# setup cluster
-helm init --client-only
-# shellcheck disable=SC1090
-source "${ROOT}/../bin/setup_cluster.sh"
-setup_e2e_cluster
-
-# setup release info
-BRANCH="latest"
-if [ "${GIT_BRANCH}" != "master" ];then
-  BRANCH_NUM=$(echo "$GIT_BRANCH" | cut -f2 -d-)
-  BRANCH="${BRANCH_NUM}-dev"
-fi
-
-# different branch tag resides in dev release directory like /latest, /1.4-dev, /1.5-dev etc.
-INSTALL_VERSION=$(curl "https://storage.googleapis.com/istio-build/dev/${BRANCH}")
-echo "Setup istio release: ${INSTALL_VERSION}"
-# TAG is of the form like "1.5-alpha.sha"
-# shellcheck disable=SC2155
-export GIT_SHA=$(echo "$TAG" | cut -f3 -d.)
-
-pushd "${ROOT}/istio-install"
-   DEV_VERSION=${INSTALL_VERSION} ./setup_istio.sh -f istioctl_profiles/default.yaml
-popd
-
-# install dependencies
-cd "${WD}/runner"
-pipenv install
-
-# setup istio test
-pushd "${WD}"
-export ISTIO_INJECT="true"
-./setup_test.sh
-popd
-dt=$(date +'%Y%m%d')
-# Current output dir should be like: 20191025_1.5-alpha.f19fb40b777e357b605e85c04fb871578592ad1e
-export OUTPUT_DIR="${dt}_${INSTALL_VERSION}"
-LOCAL_OUTPUT_DIR="/tmp/${OUTPUT_DIR}"
-mkdir -p "${LOCAL_OUTPUT_DIR}"
-
-setup_fortio_and_prometheus
-
-# add trap to copy raw data when exiting, also output logging information for debugging
-trap exit_handling ERR
-trap exit_handling EXIT
-
-echo "Start running perf benchmark test, data would be saved to GCS bucket: ${GCS_BUCKET}/${OUTPUT_DIR}"
-
-# enable flame graph
-# enable_perf_record
+# Start run perf test
+echo "Start to run perf benchmark test, all collected data will be dumped to GCS bucket: ${GCS_BUCKET}/${OUTPUT_DIR}"
 
 # For adding or modifying configurations, refer to perf/benchmark/README.md
 CONFIG_DIR="${WD}/configs/istio"
-
+# Read through perf test configuration file to determine which group of test configuration to run or not run
 read_perf_test_conf "${WD}/configs/run_perf_test.conf"
 
 for dir in "${CONFIG_DIR}"/*; do
-    # get the last directory name after splitting dir path by '/', which is the configuration dir name
+    # Get the last directory name after splitting dir path by '/', which is the configuration dir name
     config_name="$(basename "${dir}")"
-    # skip the test config that is disabled to run
+    # skip the test config which is disabled for running
     if ! ${!config_name:-false}; then
         continue
     fi
 
     pushd "${dir}"
-    # install istio with custom overlay
+
+    # Install istio with custom overlay
     if [[ -e "./installation.yaml" ]]; then
        extra_overlay="-f ${dir}/installation.yaml"
     fi
@@ -238,36 +233,41 @@ for dir in "${CONFIG_DIR}"/*; do
       DEV_VERSION=${INSTALL_VERSION} ./setup_istio.sh -f istioctl_profiles/default.yaml "${extra_overlay}"
     popd
 
-    # custom pre run
+    # Custom pre-run
     if [[ -e "./prerun.sh" ]]; then
        # shellcheck disable=SC1091
        source prerun.sh
     fi
 
-    # TRIALRUN as safe check for presubmit
+    # TRIALRUN as a pre-submit check, only run agaist the first set of enabled perf run in the perf_conf file
     if [[ "${TRIALRUN}" == "True" ]]; then
-       continue
+       run_benchmark_test "${WD}/configs/trialrun.yaml"
+       break
     fi
-    # collect config dump after prerun.sh and before test run, to verify test setup is correct
+
+    # Collect config_dump after prerun.sh and before test run, in order to verify test setup is correct
     collect_config_dump "${config_name}"
 
-    # run test and get data
+    # Run test and collect data
     if [[ -e "./cpu_mem.yaml" ]]; then
-       get_benchmark_data "${dir}/cpu_mem.yaml"
-    fi
-    if [[ -e "./latency.yaml" ]]; then
-       get_benchmark_data "${dir}/latency.yaml"
+       run_benchmark_test "${dir}/cpu_mem.yaml"
     fi
 
-    # collect clusters info after test run and before cleanup script postrun.sh
+    if [[ -e "./latency.yaml" ]]; then
+       run_benchmark_test "${dir}/latency.yaml"
+    fi
+
+    # Collect clusters info after test run and before cleanup postrun.sh run
     collect_clusters_info "${config_name}"
 
-    # custom post run
+    # Custom post run
     if [[ -e "./postrun.sh" ]]; then
        # shellcheck disable=SC1091
        source postrun.sh
     fi
+
     # TODO: can be added to shared_postrun.sh
+
     # restart proxy after each group
     kubectl exec -n "${NAMESPACE}" "${FORTIO_CLIENT_POD}" -c istio-proxy -- curl http://localhost:15000/quitquitquit -X POST
     kubectl exec -n "${NAMESPACE}" "${FORTIO_SERVER_POD}" -c istio-proxy -- curl http://localhost:15000/quitquitquit -X POST
@@ -275,11 +275,4 @@ for dir in "${CONFIG_DIR}"/*; do
     popd
 done
 
-if [[ "${TRIALRUN}" == "True" ]]; then
-   get_benchmark_data "${WD}/configs/trialrun.yaml"
-fi
-
-#echo "collect flame graph ..."
-#collect_flame_graph
-
-echo "perf benchmark test for istio is done."
+echo "Istio performance benchmark test is done!"
