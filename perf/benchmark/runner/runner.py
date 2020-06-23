83a1901c71d68b870d254a0f18644ced923cc93b
Carolyn Hu <carolynprh@google.com>
2020-06-23 15:25:42 -0700

    fix typo

--- perf/benchmark/runner/runner.py
+++ perf/benchmark/runner/runner.py
@@ -474,7 +474,7 @@ def run_perf_test(args):
             clientsidecar=args.clientsidecar,
             bothsidecar=args.bothsidecar,
             ingress=args.ingress,
-            mode=args.mode,
+            protocol_mode=args.mode,
             mesh=args.mesh,
             telemetry_mode=args.telemetry_mode,
             cacert=args.cacert,
