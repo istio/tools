4e9d615b10e82e5a9e514932b14a6f3472001e96
carolynhu <carolynprh@google.com>
2020-07-09 19:33:47 -0700

    Add fortio vs nighthawk interactive page

---
+++ perf_dashboard/fortio_vs_nighthawk/views.py
@@ -0,0 +1,33 @@
+# Copyright Istio Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from django.shortcuts import render
+import pandas as pd
+from helpers import download
+import os
+
+current_release = [os.getenv('CUR_RELEASE')]
+
+
+# Create your views here.
+def fortio_vs_nighthawk(request):
+    cur_href_links, cur_release_names, cur_release_dates, master_href_links, master_release_names, master_release_dates = download.download_benchmark_csv(60)
+    cur_release_bundle = get_artifacts_release_bundle(cur_release_dates, cur_release_names, cur_href_links)
+    master_release_bundle = get_artifacts_release_bundle(master_release_dates, master_release_names, master_href_links)
+
+    context = {'current_release': current_release,
+               'cur_release_bundle': cur_release_bundle,
+               'master_release_bundle': master_release_bundle}
+
+    return render(request, "fortio_vs_nighthawk.html", context=context)
+
