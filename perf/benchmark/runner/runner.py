df5c12d63f1ae0769e0385212674d0899e10f575
carolynhu <carolynprh@google.com>
2020-07-13 18:59:41 -0700

    add self.jitter

--- perf/benchmark/runner/runner.py
+++ perf/benchmark/runner/runner.py
@@ -324,7 +324,7 @@ class Fortio:
 
         load_gen_cmd = ""
         if self.load_gen_type == "fortio":
-            load_gen_cmd = self.generate_fortio_cmd(headers_cmd, conn, qps, duration, grpc, cacert_arg, labels)
+            load_gen_cmd = self.generate_fortio_cmd(headers_cmd, conn, qps, duration, grpc, cacert_arg, self.jitter, labels)
         elif self.load_gen_type == "nighthawk":
             # TODO(oschaaf): Figure out how to best determine the right concurrency for Nighthawk.
             # Results seem to get very noisy as the number of workers increases, are the clients
