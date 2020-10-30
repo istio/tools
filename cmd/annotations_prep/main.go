7b2da05527a9e7ffbe4ba5d01a6ffd78ad5955f1
Nathan Mittler <nmittler@gmail.com>
2020-10-30 12:20:17 -0700

    Add support for generating labels to annotations_prep (#1277)

--- cmd/annotations_prep/main.go
+++ cmd/annotations_prep/main.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Istio Authors
+// Copyright Istio Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ package main
 import (
 	"bytes"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -39,6 +40,26 @@ const (
 // GENERATED FILE -- DO NOT EDIT
 
 package {{ .Package }}
+
+type FeatureStatus int
+
+const (
+	Alpha FeatureStatus = iota
+	Beta
+	Stable
+)
+
+func (s FeatureStatus) String() string {
+	switch s {
+	case Alpha:
+		return "Alpha"
+	case Beta:
+		return "Beta"
+	case Stable:
+		return "Stable"
+	}
+	return "Unknown"
+}
 
 type ResourceTypes int
 
@@ -59,29 +80,33 @@ func (r ResourceTypes) String() string {
 	return "Unknown"
 }
 
-// Instance describes a single resource annotation
+// Instance describes a single resource {{ .Collection.NameLowercase }}
 type Instance struct {
-	// The name of the annotation.
+	// The name of the {{ .Collection.NameLowercase }}.
 	Name string
 
-	// Description of the annotation.
+	// Description of the {{ .Collection.NameLowercase }}.
 	Description string
 
-	// Hide the existence of this annotation when outputting usage information.
+	// FeatureStatus of this {{ .Collection.NameLowercase }}.
+	FeatureStatus FeatureStatus
+
+	// Hide the existence of this {{ .Collection.NameLowercase }} when outputting usage information.
 	Hidden bool
 
-	// Mark this annotation as deprecated when generating usage information.
+	// Mark this {{ .Collection.NameLowercase }} as deprecated when generating usage information.
 	Deprecated bool
 
-	// The types of resources this annotation applies to.
+	// The types of resources this {{ .Collection.NameLowercase }} applies to.
 	Resources []ResourceTypes
 }
 
 var (
-{{ range .Annotations }}
-	{{ .VariableName }} = Instance {
+{{ range .Variables }}
+	{{ .GoName }} = Instance {
 		Name: "{{ .Name }}",
 		Description: {{ wordWrap .Description 24 }},
+		FeatureStatus: {{ .FeatureStatus }},
 		Hidden: {{ .Hidden }},
 		Deprecated: {{ .Deprecated }},
 		Resources: []ResourceTypes{
@@ -93,10 +118,10 @@ var (
 {{ end }}
 )
 
-func AllResourceAnnotations() []*Instance {
+func AllResource{{ .Collection.NamePlural }}() []*Instance {
 	return []*Instance {
-		{{- range .Annotations }}
-		&{{ .VariableName }},
+		{{- range .Variables }}
+		&{{ .GoName }},
 		{{- end }}
 	}
 }
@@ -110,26 +135,27 @@ func AllResourceTypes() []string {
 }`
 
 	htmlOutputTemplate = `---
-title: Resource Annotations
-description: Resource annotations used by Istio.
-location: https://istio.io/docs/reference/config/annotations.html
+title: Resource {{ .CollectionPlural }} 
+description: Resource {{ .Collection.NameLowercasePlural }} used by Istio.
+location: {{ .HTMLLocation }}
 weight: 60
 ---
 <p>
-This page presents the various <a href="https://kubernetes.io/docs/concepts/overview/working-with-objects/annotations/">resource annotations</a> that
+This page presents the various resource <a href="{{ .Collection.ConceptLink }}">{{ .Collection.NameLowercasePlural }}</a> that
 Istio supports to control its behavior.
 </p>
 
 <table class="annotations">
 	<thead>
 		<tr>
-			<th>Annotation Name</th>
+			<th>{{ .Collection.Name }} Name</th>
+			<th>Feature Status</th>
 			<th>Resource Types</th>
 			<th>Description</th>
 		</tr>
 	</thead>
 	<tbody>
-		{{ range .Annotations }}
+		{{ range .Variables }}
 			{{ if not .Hidden }}
 				{{ if .Deprecated }}
 					<tr class="deprecated">
@@ -137,6 +163,11 @@ Istio supports to control its behavior.
 					<tr>
 				{{ end }}
 					<td><code>{{ .Name }}</code></td>
+				{{ if .Deprecated }}
+					<td>Deprecated</td>
+				{{ else }}
+					<td>{{ .FeatureStatus }}</td>
+				{{ end }}
 					<td>{{ .Resources }}</td>
 					<td>{{ .Description }}</td>
 				</tr>
@@ -147,32 +178,87 @@ Istio supports to control its behavior.
 `
 )
 
+type FeatureStatus string
+
+const (
+	Alpha  FeatureStatus = "Alpha"
+	Beta   FeatureStatus = "Beta"
+	Stable FeatureStatus = "Stable"
+)
+
+// Collection represents template fields for either annotations or labels.
+type Collection struct {
+	Name                string
+	NamePlural          string
+	NameLowercase       string
+	NameLowercasePlural string
+	// Link is the location of the generated page on istio.io.
+	Link string
+	// ConceptLink is the link to the concept page for the collection type.
+	ConceptLink string
+}
+
 var (
-	input      string
-	output     string
-	htmlOutput string
+	annotations = Collection{
+		Name:                "Annotation",
+		NamePlural:          "Annotations",
+		NameLowercase:       "annotation",
+		NameLowercasePlural: "annotations",
+		Link:                "https://istio.io/latest/docs/reference/config/annotations/",
+		ConceptLink:         "https://kubernetes.io/docs/concepts/overview/working-with-objects/annotations/",
+	}
+
+	labels = Collection{
+		Name:                "Label",
+		NamePlural:          "Labels",
+		NameLowercase:       "label",
+		NameLowercasePlural: "labels",
+		Link:                "https://istio.io/docs/reference/config/labels/",
+		ConceptLink:         "https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/",
+	}
+)
+
+func collectionForType(typ string) (Collection, error) {
+	switch typ {
+	case annotations.NameLowercase:
+		return annotations, nil
+	case labels.NameLowercase:
+		return labels, nil
+	default:
+		return Collection{}, fmt.Errorf("unrecognized variable_type: %s", typ)
+	}
+}
+
+var (
+	input          string
+	output         string
+	htmlOutput     string
+	collectionType string
+	collection     Collection
 
 	nameSeparator = regexp.MustCompile(`[._\-]`)
 
 	rootCmd = cobra.Command{
 		Use:   "annotations_prep",
-		Short: "Generates a Go source file and HTML file containing annotations.",
-		Long:  "Generates a Go source file and HTML file containing annotation definitions based on an input YAML file.",
+		Short: "Generates a Go source file and HTML file containing annotations/labels.",
+		Long: "Generates a Go source file and HTML file containing annotation/label definitions based " +
+			"on an input YAML file.",
 		Run: func(cmd *cobra.Command, args []string) {
+			processFlags()
 			yamlContent, err := ioutil.ReadFile(input)
 			if err != nil {
-				log.Fatalf("unable to read input file: %v", err)
+				log.Fatalf("unable to read input YAML file: %v", err)
 			}
 
 			// Unmarshal the file.
 			var cfg Configuration
 			if err = yaml.Unmarshal(yamlContent, &cfg); err != nil {
-				log.Fatalf("error parsing input file: %v", err)
+				log.Fatalf("error parsing input YAML file: %v", err)
 			}
 
 			// Find all the known resource types
 			m := make(map[string]bool)
-			for _, a := range cfg.Annotations {
+			for _, a := range cfg.Variables {
 				for _, r := range a.Resources {
 					m[r] = true
 				}
@@ -183,35 +269,43 @@ var (
 			}
 			sort.Strings(knownTypes)
 
-			// Generate variable names if not provided in the yaml.
-			for i := range cfg.Annotations {
-				if cfg.Annotations[i].Name == "" {
-					log.Fatalf("annotation %d in input file missing name", i)
+			// Process/cleanup the values read in from YAML.
+			for i := range cfg.Variables {
+				if cfg.Variables[i].Name == "" {
+					log.Fatalf("variable %d in input YAML file missing name", i)
 				}
-				if cfg.Annotations[i].VariableName == "" {
-					cfg.Annotations[i].VariableName = generateVariableName(cfg.Annotations[i].Name)
+
+				// Generate variable names if not provided in the yaml.
+				if cfg.Variables[i].GoName == "" {
+					cfg.Variables[i].GoName = generateVariableName(cfg.Variables[i].Name)
+				}
+
+				// Use a sensible default for feature status.
+				if cfg.Variables[i].FeatureStatus == "" {
+					cfg.Variables[i].FeatureStatus = string(generateFeatureStatus(cfg.Variables[i]))
 				}
 			}
 
 			// sort by name
-			sort.Slice(cfg.Annotations, func(i, j int) bool {
-				return strings.Compare(cfg.Annotations[i].Name, cfg.Annotations[j].Name) < 0
+			sort.Slice(cfg.Variables, func(i, j int) bool {
+				return strings.Compare(cfg.Variables[i].Name, cfg.Variables[j].Name) < 0
 			})
 
 			// Create the output file template.
-			t, err := template.New("annoTemplate").Funcs(template.FuncMap{
+			t, err := template.New("varTemplate").Funcs(template.FuncMap{
 				"wordWrap": wordWrap, "add": add,
 			}).Parse(outputTemplate)
 			if err != nil {
-				log.Fatalf("failed parsing annotation template: %v", err)
+				log.Fatalf("failed parsing variable template: %v", err)
 			}
 
 			// Generate the Go source.
 			var goSource bytes.Buffer
 			if err := t.Execute(&goSource, map[string]interface{}{
-				"Package":     getPackage(),
-				"KnownTypes":  knownTypes,
-				"Annotations": cfg.Annotations,
+				"Package":    getPackage(),
+				"KnownTypes": knownTypes,
+				"Variables":  cfg.Variables,
+				"Collection": collection,
 			}); err != nil {
 				log.Fatalf("failed generating output Go source code %s: %v", output, err)
 			}
@@ -232,8 +326,9 @@ var (
 				// Generate the HTML file.
 				var htmlFile bytes.Buffer
 				if err := t.Execute(&htmlFile, map[string]interface{}{
-					"Package":     getPackage(),
-					"Annotations": cfg.Annotations,
+					"Package":    getPackage(),
+					"Variables":  cfg.Variables,
+					"Collection": collection,
 				}); err != nil {
 					log.Fatalf("failed generating output HTML file %s: %v", output, err)
 				}
@@ -253,10 +348,21 @@ func init() {
 		"Output Go file to be generated.")
 	rootCmd.PersistentFlags().StringVar(&htmlOutput, "html_output", "",
 		"Output HTML file to be generated.")
+	rootCmd.PersistentFlags().StringVar(&collectionType, "collection_type", annotations.NameLowercase,
+		fmt.Sprintf("Output type for the generated collection. Allowed values are '%s' or '%s'.",
+			annotations.NameLowercase, labels.NameLowercase))
 
 	flag.CommandLine.VisitAll(func(gf *flag.Flag) {
 		rootCmd.PersistentFlags().AddGoFlag(gf)
 	})
+}
+
+func processFlags() {
+	var err error
+	collection, err = collectionForType(collectionType)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
@@ -265,31 +371,32 @@ func main() {
 	}
 }
 
-type AnnotationVariable struct {
-	Instance
-	VariableName string `json:"variableName"`
-}
+type Variable struct {
+	// The name of the generated golang variable.
+	GoName string `json:"variableName"`
 
-type Configuration struct {
-	Annotations []AnnotationVariable `json:"annotations"`
-}
-
-// Instance describes a single resource annotation
-type Instance struct {
-	// The name of the annotation.
+	// The name of the collection variable.
 	Name string `json:"name"`
 
-	// Description of the annotation.
+	// FeatureStatus of the collection variable.
+	FeatureStatus string `json:"featureStatus"`
+
+	// Description of the collection variable.
 	Description string `json:"description"`
 
-	// Hide the existence of this annotation when outputting usage information.
+	// Hide the existence of this collection variable when outputting usage information.
 	Hidden bool `json:"hidden"`
 
 	// Mark this annotation as deprecated when generating usage information.
 	Deprecated bool `json:"deprecated"`
 
-	// Indicates the types of resources this annotation can be applied to.
+	// Indicates the types of resources this collection variable can be applied to.
 	Resources []string `json:"resources"`
+}
+
+type Configuration struct {
+	// TODO(nmittler): refactor this so that "annotations" or "labels" can appear in the yaml.
+	Variables []Variable `json:"annotations"`
 }
 
 func getPackage() string {
@@ -330,6 +437,38 @@ func generateVariableName(annoName string) string {
 
 	// Concatenate the names together.
 	return ns + name
+}
+
+func getFeatureStatus(fs string) (FeatureStatus, error) {
+	switch FeatureStatus(strings.ToTitle(fs)) {
+	case Alpha:
+		return Alpha, nil
+	case Beta:
+		return Beta, nil
+	case Stable:
+		return Stable, nil
+	}
+	return "", fmt.Errorf("invalid feature status string: %s", fs)
+}
+
+func generateFeatureStatus(v Variable) FeatureStatus {
+	if len(v.FeatureStatus) > 0 {
+		fs, err := getFeatureStatus(v.FeatureStatus)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return fs
+	}
+
+	// If the name begins with the feature status, use it.
+	firstPart := strings.Split(v.Name, ".")
+	fs, err := getFeatureStatus(firstPart[0])
+	if err == nil {
+		return fs
+	}
+
+	// Default to Alpha
+	return Alpha
 }
 
 func wordWrap(in string, indent int) string {
