365d62a207327f30a2b1ce92f73b2142a1a4bac1
Yuchen Dai <lambdai@google.com>
2021-06-07 21:53:59 +0000

    remove duplicate ln -s python3
    Signed-off-by: Yuchen Dai <lambdai@google.com>

--- docker/build-tools/install-python-3.8.sh
+++ docker/build-tools/install-python-3.8.sh
@@ -21,5 +21,5 @@ apt-get install -y make build-essential libssl-dev zlib1g-dev \
         libncurses5-dev libncursesw5-dev xz-utils tk-dev
 wget https://www.python.org/ftp/python/3.8.10/Python-3.8.10.tgz
 tar zxvf Python-3.8.10.tgz
-cd Python-3.8.10 && ./configure --with-ensurepip=install && make -j 16 && make install && ln -s  /usr/local/bin/python3 /usr/bin/python
+cd Python-3.8.10 && ./configure --with-ensurepip=install && make -j 16 && make install
 cd .. && rm -rf ./Python-3.8.10 && rm -rf ./Python-3.8.10.tgz
