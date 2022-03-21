1946048c92c06ddce7bce983b535081672b85220
Xunzhuo <mixdeers@gmail.com>
2022-03-21 12:31:36 +0800

    fix: lint errors
    Signed-off-by: Xunzhuo <mixdeers@gmail.com>

--- perf/load/loadclient/setup_test.sh
+++ perf/load/loadclient/setup_test.sh
@@ -34,7 +34,7 @@ if [[ -z "${GATEWAY_URL:-}" ]];then
   if [[ -z "${GATEWAY_SERVICE_NAME:-}" ]];then
     GATEWAY_URL=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}' || true)
   else
-    GATEWAY_URL=$(kubectl -n istio-system get service ${GATEWAY_SERVICE_NAME} -o jsonpath='{.status.loadBalancer.ingress[0].ip}' || true)
+    GATEWAY_URL=$(kubectl -n istio-system get service "${GATEWAY_SERVICE_NAME}" -o jsonpath='{.status.loadBalancer.ingress[0].ip}' || true)
   fi
 fi
 
