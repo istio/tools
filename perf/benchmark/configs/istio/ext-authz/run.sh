767cfb8598824000c49589d6fe6594c7b77d43cb
bochengchu <cbchu1117@gmail.com>
2022-09-28 23:31:19 +0000

    small/med/large loads

--- perf/benchmark/configs/istio/ext-authz/run.sh
+++ perf/benchmark/configs/istio/ext-authz/run.sh
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-BENCHMARK_DIR=$(dirname "$0")/../../..
 CONFIG_DIR=$(dirname "$0")
 FORTIOCLIENT=$(kubectl get pods -n twopods-istio --selector=app=fortioclient --output=jsonpath={.items..metadata.name})
 PROVIDER=$(kubectl get services -n twopods-istio --selector=app=ext-authz --output=jsonpath={.items..spec.clusterIP})
@@ -23,32 +22,46 @@ PROVIDER=$(kubectl get services -n twopods-istio --selector=app=ext-authz --outp
 kubectl delete -n twopods-istio -f ${CONFIG_DIR}/policy.yaml || true
 
 # client to server, without ext-authz
-python3 ${BENCHMARK_DIR}/runner/runner.py \
-    --config_file=${CONFIG_DIR}/no_ext-authz_multi_conn_latency.yaml
-python3 ${BENCHMARK_DIR}/runner/runner.py \
-    --config_file=${CONFIG_DIR}/no_ext-authz_multi_qps_latency.yaml
+kubectl -n twopods-istio exec ${FORTIOCLIENT}  \
+    -- fortio load -jitter=True -c 8 -qps 100 \
+    -t 300s -a -r 0.001 -httpbufferkb=128 \
+    -labels qps_100_c_8_without-ext-authz_small http://fortioserver:8080/echo?size=1024
+kubectl -n twopods-istio exec ${FORTIOCLIENT}  \
+    -- fortio load -jitter=True -c 32 -qps 500 \
+    -t 300s -a -r 0.001 -httpbufferkb=128 \
+    -labels qps_500_c_32_without-ext-authz_medium http://fortioserver:8080/echo?size=1024
+kubectl -n twopods-istio exec ${FORTIOCLIENT}  \
+    -- fortio load -jitter=True -c 64 -qps 1000 \
+    -t 300s -a -r 0.001 -httpbufferkb=128 \
+    -labels qps_1000_c_64_without-ext-authz_large http://fortioserver:8080/echo?size=1024
 
 kubectl apply -n twopods-istio -f ${CONFIG_DIR}/policy.yaml
 
 # client to server, with ext-authz
-python3 ${BENCHMARK_DIR}/runner/runner.py \
-    --config_file=${CONFIG_DIR}/with_ext-authz_multi_conn_latency.yaml
-python3 ${BENCHMARK_DIR}/runner/runner.py \
-    --config_file=${CONFIG_DIR}/with_ext-authz_multi_qps_latency.yaml
+kubectl -n twopods-istio exec ${FORTIOCLIENT}  \
+    -- fortio load -H=x-ext-authz:allow -jitter=True -c 8 -qps 100 \
+    -t 300s -a -r 0.001 -httpbufferkb=128 \
+    -labels qps_100_c_8_with-ext-authz_small http://fortioserver:8080/echo?size=1024
+kubectl -n twopods-istio exec ${FORTIOCLIENT}  \
+    -- fortio load -H=x-ext-authz:allow -jitter=True -c 32 -qps 500 \
+    -t 300s -a -r 0.001 -httpbufferkb=128 \
+    -labels qps_500_c_32_with-ext-authz_medium http://fortioserver:8080/echo?size=1024
+kubectl -n twopods-istio exec ${FORTIOCLIENT}  \
+    -- fortio load -H=x-ext-authz:allow -jitter=True -c 64 -qps 1000 \
+    -t 300s -a -r 0.001 -httpbufferkb=128 \
+    -labels qps_1000_c_64_with-ext-authz_large http://fortioserver:8080/echo?size=1024
 
 # client to ext-authz provider
-for conn in 2 4 8 16 32
-do
-    kubectl -n twopods-istio exec ${FORTIOCLIENT}  \
-        -- fortio load -H=x-ext-authz:allow  -jitter=True -c $conn -qps 100 \
-        -t 100s -a -r 0.001 -httpbufferkb=128 \
-        -labels qps_100_c_${conn}_v2-stats-nullvm_to-ext-authz_both http://${PROVIDER}:8000/echo?size=1024
-done
+kubectl -n twopods-istio exec ${FORTIOCLIENT}  \
+    -- fortio load -H=x-ext-authz:allow -jitter=True -c 8 -qps 100 \
+    -t 300s -a -r 0.001 -httpbufferkb=128 \
+    -labels qps_100_c_8_to-ext-authz_small http://${PROVIDER}:8000/echo?size=1024
+kubectl -n twopods-istio exec ${FORTIOCLIENT}  \
+    -- fortio load -H=x-ext-authz:allow -jitter=True -c 32 -qps 500 \
+    -t 300s -a -r 0.001 -httpbufferkb=128 \
+    -labels qps_500_c_32_to-ext-authz_medium http://${PROVIDER}:8000/echo?size=1024
+kubectl -n twopods-istio exec ${FORTIOCLIENT}  \
+    -- fortio load -H=x-ext-authz:allow -jitter=True -c 64 -qps 1000 \
+    -t 300s -a -r 0.001 -httpbufferkb=128 \
+    -labels qps_1000_c_64_to-ext-authz_large http://${PROVIDER}:8000/echo?size=1024
 
-for qps in 100 200 300 400 500
-do
-    kubectl -n twopods-istio exec ${FORTIOCLIENT}  \
-        -- fortio load -H=x-ext-authz:allow  -jitter=True -c 64 -qps $qps \
-        -t 100s -a -r 0.001 -httpbufferkb=128 \
-        -labels qps_${qps}_c_64_1024_v2-stats-nullvm_to-ext-authz_both http://${PROVIDER}:8000/echo?size=1024
-done
