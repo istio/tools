78404a7c2ab34713b3757176404835855d2e63cb
John Howard <howardjohn@google.com>
2023-10-02 10:46:09 -0700

    crd gen: hide from docs regardless of where in the comment it is

--- cmd/cue-gen/genoapi.go
+++ cmd/cue-gen/genoapi.go
@@ -426,7 +426,7 @@ func (x *builder) genOpenAPI(name string, inst cue.Value) (cue.Value, error) {
 			if doc.Text() == "" {
 				continue
 			}
-			if strings.HasPrefix(doc.Text(), "$hide_from_docs") {
+			if strings.Contains(doc.Text(), "$hide_from_docs") {
 				return ""
 			}
 			if paras := strings.Split(doc.Text(), "\n"); len(paras) > 0 {
