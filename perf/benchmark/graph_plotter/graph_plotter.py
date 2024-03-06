1beccaeb32438d7238d8c5d0e5178d5b6f319d03
Shuayb Popoola <34720754+Shuayb1@users.noreply.github.com>
2024-03-06 16:36:13 +0000

    Delete perf option (#2843)
    * added optional flag to delete previous perf data before generating a new one
    
    * added optional flag to delete previous perf data before generating a new one
    
    * delete preivious perf data before running new test, re-added side to the perf label and label graph
    
    * delete preivious perf data before running new test, re-added side to the perf label and label graph

--- perf/benchmark/graph_plotter/graph_plotter.py
+++ perf/benchmark/graph_plotter/graph_plotter.py
@@ -42,7 +42,13 @@ def plotter(args):
     ax = fig.add_subplot(111)
     ax.set_ylim(0, 1.0)
     for key, val in telemetry_modes_y_data.items():
-        plt.plot(args.query_list, val, marker='o', label=key)
+        plot_key = key
+        match key:
+            case "no_istio_mtls_no_istio":
+                plot_key = "no_istio"
+            case "istio_with_stats_both":
+                plot_key = "istio_with_stats"
+        plt.plot(args.query_list, val, marker='o', label=plot_key)
         for i, j in zip(args.query_list, val):
             ax.annotate(str(j), xy=(i, j))
             print("i=%x,j=%x,args.querylist=%x,val=%x,key=%x", i, j, args.query_list, val, key)
