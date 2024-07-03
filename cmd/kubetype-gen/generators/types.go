cfba86f7b0bd808da796eda8d8e285f22a3d6f8d
ilrudie <ian.rudie@solo.io>
2024-07-03 11:10:31 -0400

    adding SE status generation
    Signed-off-by: ilrudie <ian.rudie@solo.io>

--- cmd/kubetype-gen/generators/types.go
+++ cmd/kubetype-gen/generators/types.go
@@ -53,20 +53,25 @@ func (g *typesGenerator) GenerateType(c *generator.Context, t *types.Type, w io.
 	kubeTypes := g.source.KubeTypes(t)
 	sw := generator.NewSnippetWriter(w, c, "$", "$")
 	m := map[string]interface{}{
-		"KubeType":    nil,
-		"RawType":     t,
-		"TypeMeta":    c.Universe.Type(types.Name{Name: "TypeMeta", Package: "k8s.io/apimachinery/pkg/apis/meta/v1"}),
-		"ObjectMeta":  c.Universe.Type(types.Name{Name: "ObjectMeta", Package: "k8s.io/apimachinery/pkg/apis/meta/v1"}),
-		"ListMeta":    c.Universe.Type(types.Name{Name: "ListMeta", Package: "k8s.io/apimachinery/pkg/apis/meta/v1"}),
-		"IstioStatus": c.Universe.Type(types.Name{Name: "IstioStatus", Package: "istio.io/api/meta/v1alpha1"}),
+		"KubeType":                nil,
+		"RawType":                 t,
+		"TypeMeta":                c.Universe.Type(types.Name{Name: "TypeMeta", Package: "k8s.io/apimachinery/pkg/apis/meta/v1"}),
+		"ObjectMeta":              c.Universe.Type(types.Name{Name: "ObjectMeta", Package: "k8s.io/apimachinery/pkg/apis/meta/v1"}),
+		"ListMeta":                c.Universe.Type(types.Name{Name: "ListMeta", Package: "k8s.io/apimachinery/pkg/apis/meta/v1"}),
+		"IstioStatus":             c.Universe.Type(types.Name{Name: "IstioStatus", Package: "istio.io/api/meta/v1alpha1"}),
+		"IstioServiceEntryStatus": c.Universe.Type(types.Name{Name: "IstioServiceEntryStatus", Package: "istio.io/api/meta/v1alpha1"}),
 	}
 	for _, kubeType := range kubeTypes {
+		template := kubeTypeTemplate
+		if kubeType.Type().Name.Name == "ServiceEntry" {
+			template = kubeTypeTemplateServiceEntry
+		}
 		// make sure local types get imports generated for them to prevent reusing their local name for real imports,
 		// e.g. generating into package v1alpha1, while also importing from another package ending with v1alpha1.
 		// adding the import here will ensure the imports will be something like, precedingpathv1alpha1.
 		g.imports.AddType(kubeType.Type())
 		m["KubeType"] = kubeType
-		sw.Do(kubeTypeTemplate, m)
+		sw.Do(template, m)
 	}
 	return sw.Error()
 }
@@ -93,6 +98,41 @@ type $.KubeType.Type|public$ struct {
 	Spec $.RawType|raw$ ` + "`" + `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"` + "`" + `
 
 	Status $.IstioStatus|raw$ ` + "`" + `json:"status"` + "`" + `
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// $.KubeType.Type|public$List is a collection of $.KubeType.Type|publicPlural$.
+type $.KubeType.Type|public$List struct {
+	$.TypeMeta|raw$ ` + "`" + `json:",inline"` + "`" + `
+	// +optional
+	$.ListMeta|raw$ ` + "`" + `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"` + "`" + `
+	Items           []*$.KubeType.Type|raw$ ` + "`" + `json:"items" protobuf:"bytes,2,rep,name=items"` + "`" + `
+}
+`
+
+const kubeTypeTemplateServiceEntry = `
+$- range .RawType.SecondClosestCommentLines $
+// $ . $
+$- end $
+$- range .KubeType.Tags $
+// +$ . $
+$- end $
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+$ range .RawType.CommentLines $
+// $ . $
+$- end $
+type $.KubeType.Type|public$ struct {
+	$.TypeMeta|raw$ ` + "`" + `json:",inline"` + "`" + `
+	// +optional
+	$.ObjectMeta|raw$ ` + "`" + `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"` + "`" + `
+
+	// Spec defines the implementation of this definition.
+	// +optional
+	Spec $.RawType|raw$ ` + "`" + `json:"spec,omitempty" protobuf:"bytes,2,opt,name=spec"` + "`" + `
+
+	Status $.IstioServiceEntryStatus|raw$ ` + "`" + `json:"status"` + "`" + `
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
