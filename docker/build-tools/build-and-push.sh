fcaa3b78086a9e80be9e92be35525c9c73136d82
Gustavo <grnmeira@gmail.com>
2025-07-07 15:08:26 +0000

    adding mingw image to build-and-push.sh

--- docker/build-tools/build-and-push.sh
+++ docker/build-tools/build-and-push.sh
@@ -90,6 +90,16 @@ ${CONTAINER_CLI} ${CONTAINER_BUILDER} --target build_env_proxy \
   -t "${HUB}/build-tools-proxy:${VERSION}-${ARCH}" \
   .
 
+# shellcheck disable=SC2086
+${CONTAINER_CLI} ${CONTAINER_BUILDER} --target build_tools \
+  ${ADDITIONAL_BUILD_ARGS} --build-arg "ISTIO_TOOLS_SHA=${SHA}" --build-arg "VERSION=${VERSION}" \
+  --build-arg BUILDKIT_INLINE_CACHE=1 \
+  --build-arg BASE_OS_CONTEXT=base_os_context_with_mingw \
+  --cache-from "${HUB}/build-tools${CACHE_FROM_TAG}" \
+  -t "${HUB}/build-tools-mingw:${BRANCH}-latest-${ARCH}" \
+  -t "${HUB}/build-tools-mingw:${VERSION}-${ARCH}" \
+  .
+
 
 if [[ -z "${DRY_RUN:-}" ]]; then
   TO_PUSH=(
@@ -97,6 +107,8 @@ if [[ -z "${DRY_RUN:-}" ]]; then
     "${HUB}/build-tools:${BRANCH}-latest"
     "${HUB}/build-tools-proxy:${VERSION}"
     "${HUB}/build-tools-proxy:${BRANCH}-latest"
+    "${HUB}/build-tools-mingw:${VERSION}"
+    "${HUB}/build-tools-mingw:${BRANCH}-latest"
   )
   # First, push the architecture specific images
   for image in "${TO_PUSH[@]}"; do
