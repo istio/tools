6db0b4c7fbe345bd531a07fbcff13877dc9cd801
John Howard <john.howard@solo.io>
2026-06-14 00:07:32 -0700

    protoc-gen-golang-jsonshim: mark optional as supported (#3420)

--- cmd/protoc-gen-golang-jsonshim/main.go
+++ cmd/protoc-gen-golang-jsonshim/main.go
@@ -20,10 +20,12 @@ import (
 
 	"google.golang.org/protobuf/compiler/protogen"
 	"google.golang.org/protobuf/types/descriptorpb"
+	"google.golang.org/protobuf/types/pluginpb"
 )
 
 func main() {
 	protogen.Options{}.Run(func(gen *protogen.Plugin) error {
+		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
 		for _, f := range gen.Files {
 			if !f.Generate {
 				continue
